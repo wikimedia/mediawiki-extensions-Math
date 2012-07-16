2b3030bd7eab86b450a9db6a9862f16fa5619ded
Physikerwelt <wiki@physikerwelt.de>
2012-07-16 12:48:29 +0200

    adjusted to the actual version of LaTeXML

--- Math.LaTeXML.php
+++ Math.LaTeXML.php
@@ -14,16 +14,47 @@ class MathLaTeXML{
 	function render($super) {
 			global $wgLaTeXMLUrl;
 
-
 			$texcmd=urlencode("\$".$super->tex."\$");
+			$post="summary=true&profile=fragment&tex=$texcmd";
+			$time_start = microtime(true);
+			$res= Http::post($wgLaTeXMLUrl, array("postData"=> $post,"timeout"=>60));
+			$time_end = microtime(true);
+			$time = $time_end - $time_start;
+			$result=json_decode($res);
+			// if ($result->result){
+			// $super->mathml= $result->result;
+			// }else{
+				// var_dump($result);
+				// var_dump($post);
+			// }
+			if($result->result){//$result&&is_array($result)&&is_array($result['result'])&&count($result['result'])>0){
+				if($result->status!="No obvious problems"){
+				$super->status= $result->status;
+				$super->log =$result->log;}
+				$super->mathml= $result->result;}
+			else
+				{wfDebug("LaTeXML","\nLaTeXML Error:". var_export(array($result,$post, $wgLaTeXMLUrl),true)."\n\n");
+				return false;}
+
+			wfDebug( "Latexml request: $post\n processed in $time seconds." );
+			return true;
+			//wfDebug( "Latexml output:\n $result\n---\n" );
+		}
+		static function LaTeXMLRender($URL,$texcmd){
+
+			$texcmd=urlencode("\$".$texcmd>tex."\$");
 			$post="profile=math&tex=$texcmd";
 
-			$result= EDUtils::getDataFromURL( $wgLaTeXMLUrl, "json", $post );
-
+			$result= EDUtils::getDataFromURL( $URL, "json", $post );
+			if (is_array($result['result'])){
 			$super->mathml= $result['result'][0];
-
+			}else{
+				var_dump($result);
+				var_dump($post);
+			}
 			wfDebug( "Latexml request: $post\n" );
 			wfDebug( "Latexml output:\n $result\n---\n" );
+
 		}
 		
 }
