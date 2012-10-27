ed4e704bc6218ae5f6ca1774999a9faf57a8b0ac
Physikerwelt <wiki@physikerwelt.de>
2012-10-27 16:30:50 +0200

    Restructuring Math classes
    The Math.body file which contains the MathRender class was split in the following way:
    - Math.base contains the base class with the database related stuff and provides an abstract interface
    - Math.source and Math.MathJax handle the plain tex string output. There are two classes since they
      will differntiate in the future I think.
    - Math.texvc contains the "old" implementation of png generation with all the file handling related stuff
    - Other implementation of math renderer can be added in the same style.
    
    The first attempt restructure the class layout and introduce LaTeXML at the same
    time was dropped. Instead this was split up into two phases.
    This commit only deals about the restructuring of the math module design.
    
    Change-Id: I9b1d68c4faa8d177d8d0088fa1a5879caed4f1fe

---
+++ MathMathJax.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * MediaWiki math extension
+ *
+ * (c) 2002-2012 Tomasz Wegrzanowski, Brion Vibber, Moritz Schubotz and other MediaWiki contributors
+ * GPLv2 license; info in main package.
+ *
+ * Contains everything related to <math> </math> parsing
+ * @file
+ */
+
+
+/**
+ * Takes LaTeX fragments and outputs the source directly to the browser
+ *
+ * @author Tomasz Wegrzanowski, with additions by Brion Vibber (2003, 2004)
+ * @ingroup Parser
+ */
+class MathMathJax extends MathRenderer {
+
+	function render() {
+			# No need to render or parse anything more!
+			# New lines are replaced with spaces, which avoids confusing our parser (bugs 23190, 22818)
+			return Xml::element( 'span',
+				$this->_attribs(
+					'span',
+					array(
+						'class' => 'tex',
+						'dir' => 'ltr'
+					)
+				),
+				'$ ' . str_replace( "\n", " ", $this->tex ) . ' $'
+			);
+		}
+
+
+
+}
