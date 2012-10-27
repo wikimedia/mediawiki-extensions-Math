627549c121a4fef7f490f2bd77746c808f56f486
Physikerwelt <wiki@physikerwelt.de>
2012-10-27 14:22:14 +0200

    Some minor corrections - introduced getTex instead of access to the public var $tex - MathJaX libary is only loaded if not Firefox is beeing used - 'math_outputhash' is not set to null if converted via LaTeXML

--- MathLaTeXML.php
+++ MathLaTeXML.php
@@ -29,8 +29,10 @@ class MathLaTeXML extends MathRenderer {
 	 * @see MathRenderer::writeCache()
 	 */
 	function writeCache() {
-		if ( !$this->isRecall() )
+		if ( !$this->isRecall() ){
+			$this->hash=0;
 			$this->_writeDBentry();
+		}
 	}
 	/**
 	 * Picks a LaTeXML daemon if more than one are availible from the $wgLaTeXMLUrl array
