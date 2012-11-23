b1a91d32cf0cd392d7c7f3edad39a8baef4ed0a9
physikerwelt <wiki@physikerwelt.de>
2012-11-23 11:47:31 +0100

    -Change of the parameters to the hook on formula rendered. -Remove of page_id and anchor_id from MathRenderer.php (since this is not related to the equation itself) -Implementation of math debug mode $wgDebugMath

--- MathLaTeXML.php
+++ MathLaTeXML.php
@@ -18,8 +18,9 @@ class MathLaTeXML extends MathRenderer {
 	 * @see MathRenderer::render()
 	*/
 	function render($purge=false) {
-		if ( $purge||!$this->_readFromDB() || $this->mathml == "" || $this->isPurge() ) { // ||
+		if ( $purge||!$this->_readFromDB() || !self::isValidMathML($this->mathml) || $this->isPurge() ) { // ||
 			wfDebugLog( "Math", "no recall" );
+			$this->recall=false;
 			$this->dorender();
 		}
 		return $this->_embedMathML();
@@ -56,8 +57,9 @@ class MathLaTeXML extends MathRenderer {
 		global $wgDebugMath;
 
 		$host = self::pickHost();
-		$texcmd = "literal:" . urlencode( "\$" . $this->tex . "\$" );
-		$post = "profile=math&tex=$texcmd";
+		$this->tex=str_replace('\\$','\$',str_replace('$','\$',$this->tex));
+		$texcmd = 'literal:' . urlencode( $this->tex );
+		$post = 'preload=texvc.sty&profile=math&tex='.$texcmd;
 		$time_start = microtime( true );
 		$res = Http::post( $host, array( "postData" => $post, "timeout" => 60 ) );
 		$time_end = microtime( true );
@@ -126,7 +128,7 @@ class MathLaTeXML extends MathRenderer {
 	 * @return string
 	 */
 	private function _embedMathML() {
-		return self::embedMathML($this->mathml, $this->getAnchorID());
+		return self::embedMathML($this->mathml);
 	}
 	public static function embedMathML($mml,$anchorID=0){
 		$mml = str_replace( "\n", " ", $mml );
@@ -134,8 +136,8 @@ class MathLaTeXML extends MathRenderer {
 				self::attribs( 'span',
 						array(
 								'class' => 'tex',
-								'dir' => 'ltr',
-								'id' => 'math' . $anchorID
+								'dir' => 'ltr'
+								//'id' => 'math' . urlencode($this->tex)
 						)
 				),
 				$mml
