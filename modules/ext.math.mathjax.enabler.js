ca6a896cb081668fe37f141c6dd71784f3c611c7
Antoine Musso <hashar@free.fr>
2013-01-18 12:48:25 +0100

    JSHint: Fix quotmark and semicolon warnings.
    Change-Id: I2ab9e7ce308fd47336f86d2571e8450347321f10

--- modules/ext.math.mathjax.enabler.js
+++ modules/ext.math.mathjax.enabler.js
@@ -2,21 +2,21 @@
  * From https://en.wikipedia.org/wiki/User:Nageh/mathJax.js
  */
 
-if ( typeof(mathJax) === "undefined" ) mathJax = {};
+if ( typeof mathJax === 'undefined' ) mathJax = {};
 
-mathJax.version = "0.2";
+mathJax.version = '0.2';
 
 mathJax.loaded = false;
 
 mathJax.Config = function() {
   MathJax.Hub.Config({
     root: mediaWiki.config.get('wgExtensionAssetsPath') + '/Math/modules/MathJax',
-    config: ["TeX-AMS-texvc_HTML.js"],
-    "v1.0-compatible": false,
-    styles: { ".mtext": { "font-family": "sans-serif ! important", "font-size": "80%" } },
-    displayAlign: "left",
-    menuSettings: { zoom: "Click" },
-    "HTML-CSS": { imageFont: null, availableFonts: ["TeX"] }
+    config: ['TeX-AMS-texvc_HTML.js'],
+    'v1.0-compatible': false,
+    styles: { '.mtext': { 'font-family': 'sans-serif ! important', 'font-size': '80%' } },
+    displayAlign: 'left',
+    menuSettings: { zoom: 'Click' },
+    'HTML-CSS': { imageFont: null, availableFonts: ['TeX'] }
   });
   MathJax.OutputJax.fontDir = mathJax.fontDir = mediaWiki.config.get('wgExtensionAssetsPath') + '/Math/modules/MathJax/fonts';
 };
@@ -41,20 +41,20 @@ mathJax.Load = function(element) {
 };
 
 mathJax.Init = function() {
-  this.Load( document.getElementById("bodyContent") || document.body );
+  this.Load( document.getElementById('bodyContent') || document.body );
 
   // compatibility with wikEd
-  if ( typeof(wikEd) == "undefined" ) { wikEd = {}; }
-  if ( typeof(wikEd.config) == "undefined" ) { wikEd.config = {}; }
-  if ( typeof(wikEd.config.previewHook) == "undefined" ) { wikEd.config.previewHook = []; }
-  wikEd.config.previewHook.push( function(){ if (window.mathJax.Load(document.getElementById("wikEdPreviewBox") || document.body)) MathJax.Hub.Queue(["Typeset", MathJax.Hub, "wikEdPreviewBox"]) } );
+  if ( typeof(wikEd) == 'undefined' ) { wikEd = {}; }
+  if ( typeof(wikEd.config) == 'undefined' ) { wikEd.config = {}; }
+  if ( typeof(wikEd.config.previewHook) == 'undefined' ) { wikEd.config.previewHook = []; }
+  wikEd.config.previewHook.push( function(){ if (window.mathJax.Load(document.getElementById('wikEdPreviewBox') || document.body)) MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'wikEdPreviewBox']); } );
 
   // compatibility with ajaxPreview
   this.oldAjaxPreviewExec = window.ajaxPreviewExec;
   window.ajaxPreviewExec = function(previewArea) {
-    if ( typeof(mathJax.oldAjaxPreviewExec) !== "undefined" ) mathJax.oldAjaxPreviewExec(previewArea);
-    if ( mathJax.Load(previewArea) ) MathJax.Hub.Queue( ["Typeset", MathJax.Hub, previewArea] );
-  }
+    if ( typeof(mathJax.oldAjaxPreviewExec) !== 'undefined' ) mathJax.oldAjaxPreviewExec(previewArea);
+    if ( mathJax.Load(previewArea) ) MathJax.Hub.Queue( ['Typeset', MathJax.Hub, previewArea] );
+  };
 };
 
 jQuery( document ).ready( function() {
