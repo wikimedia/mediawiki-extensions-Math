fa3121c910b09740f6dd4010da2c0bc6df11c978
physikerwelt <wiki@physikerwelt.de>
2013-04-25 17:07:25 +0000

    introduce setter and getter
    introduce getter and setter for the MathRenderer
    in preperation for determining if a value was changed and has
    to be cached
    
    Change-Id: Ica15f77d96453d30edd3a117c7185c694ad3691e

--- tests/MathDatabaseTest.php
+++ tests/MathDatabaseTest.php
@@ -51,9 +51,9 @@ class MathDatabaseTest extends MediaWikiTestCase {
 	 */
 	public function setValues() {
 		// set some values
-		$this->renderer->tex = self::SOME_TEX ;
-		$this->renderer->html = self::SOME_HTML;
-		$this->renderer->mathml = self::SOME_MATHML;
+		$this->renderer->SetTex( self::SOME_TEX );
+		$this->renderer->SetHtml( self::SOME_HTML );
+		$this->renderer->SetMathml( self::SOME_MATHML );
 	}
 	/**
 	 * Checks database access. Writes an etry and reads it back.
