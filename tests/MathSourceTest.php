10aea505516ad92cbf98f923982c25ba88ea7870
physikerwelt <wiki@physikerwelt.de>
2013-05-04 10:43:14 +0000

    rename MathSourceTest::simpleTest->MathSourceTest::testBasics
    Test was not executed, because it did not start with 'test'.
    
    Change-Id: Ie5e14b0a23da6a908781f9a2f17dcd6c7795bebe

--- tests/MathSourceTest.php
+++ tests/MathSourceTest.php
@@ -9,7 +9,7 @@ class MathSourceTest extends MediaWikiTestCase {
 	 * Checks the basic functionallity
 	 * i.e. if the span element is generated right.
 	 */
-	public function simpleTest(){
+	public function testBasics(){
 		$real=MathRenderer::renderMath("a+b",array(),MW_MATH_SOURCE);
 		$this->assertEquals('<span class="tex" dir="ltr">$ a+b $</span>', $real
 			, "Rendering of a+b in plain Text mode");
