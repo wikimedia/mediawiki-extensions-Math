be7633805cc83a2c19dfcd8d830cd927d5cfffa3
physikerwelt <wiki@physikerwelt.de>
2013-05-09 17:16:06 +0200

    New rendering option LaTeXML
    Do not +2!
    TODOs:
    - Fix HTTP Mock up in phpunittest.
    - Ensure proper encoding with all DataBases(utf8decode)
    
    Change-Id: I5d29e219c0d3b907e22ea0bb3b30f000d8a7a9f8

---
+++ MathLaTeXML.php
@@ -0,0 +1,258 @@
+<?php
+/**
+ * MediaWiki math extension
+ *
+ * (c)2012 Moritz Schubotz
+ * GPLv2 license; info in main package.
+ *
+ * Contains the driver function for the LaTeXML daemon
+ * @file
+ */
+
+class MathLaTeXML extends MathRenderer {
+
+	/**
+	 * @var String settings for LaTeXML daemon
+	 */
+	private $LaTeXMLSettings = '';
+
+	/**
+	 * Gets the settings for the LaTeXML daemon.
+	 *
+	 * @return string
+	 */
+	public function getLaTeXMLSettings() {
+		if ( $this->LaTeXMLSettings ) {
+			return $this->LaTeXMLSettings;
+		} else {
+			return 'format=xhtml&' .
+				'whatsin=math&' .
+				'whatsout=math&' .
+				'pmml&' . // presentation MathML
+				'cmml&' . // content MathML
+				'preload=LaTeX.pool&' .
+				'preload=article.cls&' .
+				'preload=amsmath&' .
+				'preload=amsthm&' .
+				'preload=amstext&' .
+				'preload=amssymb&' .
+				'preload=eucal&' .
+				'preload=[dvipsnames]xcolor&' .
+				'preload=url&' .
+				'preload=hyperref&' .
+				'preload=mws&' .
+				'preload=ids&' . // generate ids for parallel markup
+				'preload=texvc';
+		}
+	}
+
+	/**
+	 * Sets the settings for the LaTeXML daemon.
+	 * The settings affect only the current instance of the class.
+	 * For a list of possible settings see:
+	 * http://dlmf.nist.gov/LaTeXML/manual/commands/latexmlpost.xhtml
+	 * @param string $settings
+	 */
+	public function setLaTeXMLSettings( $settings ) {
+		$this->LaTeXMLSettings = $settings;
+	}
+	/* (non-PHPdoc)
+	 * @see MathRenderer::render()
+	*/
+	public function render( $forceReRendering = false ) {
+		if ( $forceReRendering ) {
+			$this->setPurge( $forceReRendering );
+		}
+		if ( $this->renderingRequired() ) {
+			$res = $this->doRender($error);
+			if ( ! $res ) {
+				return $error;
+			}
+		}
+		return $this->getMathMLTag();
+	}
+	/**
+	 * Helper function to checks if the math tag must be rendered.
+	 * @return boolean
+	 */
+	private function renderingRequired() {
+		if ( $this->isPurge() ) {
+			wfDebugLog( "Math", "Rerendering was requested." );
+			return true;
+		} else {
+			$dbres = $this->readFromDatabase();
+			if ( $dbres ) {
+				if ( self::isValidMathML( $this->getMathml() ) ) {
+					wfDebugLog( "Math", "Valid entry found in database." );
+					return false;
+				} else {
+					wfDebugLog( "Math", "Malformatted entry found in database" );
+					return true;
+				}
+			} else {
+				wfDebugLog( "Math", "No entry found in database." );
+				return true;
+			}
+		}
+	}
+
+	/**
+	 * Performs a HTTP Post request to the given host.
+	 * Uses $wgLaTeXMLTimeout as timeout.
+	 * Generates error messages on failure
+	 * @see Http::post()
+	 *
+	 * @param string $host
+	 * @param string $post the encoded post request
+	 * @param mixed $res the result
+	 * @param mixed $error the formatted error message or null
+	 * @return boolean success
+	 */
+	public function makeRequest( $host, $post, &$res, &$error = null ) {
+		global $wgLaTeXMLTimeout;
+		$error = null;
+		$time_start = microtime( true );
+		$res = Http::post( $host, array( "postData" => $post,
+				"timeout" => $wgLaTeXMLTimeout ) );
+		$time_end = microtime( true );
+		$time = $time_end - $time_start;
+		if ( $wgLaTeXMLTimeout > 0 && $time > $wgLaTeXMLTimeout ) {
+			$error = $this->getError( 'math_latexml_timeout', $host );
+			wfDebugLog( "Math", "\nLaTeXML Timeout:"
+				. var_export( array( 'post' => $post, 'host' => $host
+					, 'wgLaTeXMLTimeout' => $wgLaTeXMLTimeout ), true ) . "\n\n" );
+			return false;
+		} else {
+			wfDebugLog( "Math", "Latexml request: $post\n processed in $time seconds." );
+		}
+		if ( $res ) {
+			return true;
+		} else {
+			$error = $this->getError( 'latexml_invalidresponse', $host );
+			wfDebugLog( "Math", "\nLaTeXML NoResponse:"
+					. var_export( array( 'post' => $post, 'host' => $host ), true ) . "\n\n" );
+			return false;
+		}
+	}
+	/* (non-PHPdoc)
+	 * @see MathRenderer::writeCache()
+	*/
+	public function writeCache() {
+		if ( $this->isChanged() ) {
+			$this->writeToDatabase();
+		}
+	}
+
+	/**
+	 * Picks a LaTeXML daemon.
+	 * If more than one demon are availible one is chosen from the
+	 * $wgLaTeXMLUrl array.
+	 * @return string
+	 */
+	private static function pickHost() {
+		global $wgLaTeXMLUrl;
+		if ( is_array( $wgLaTeXMLUrl ) ) {
+			$pick = mt_rand( 0, count( $wgLaTeXMLUrl ) - 1 );
+			$host = $wgLaTeXMLUrl[$pick];
+		} else {
+			$host = $wgLaTeXMLUrl;
+		}
+		wfDebugLog( "Math", "picking host " . $host );
+		return $host;
+	}
+
+	/**
+	 * Does the actual web request to convert TeX to MathML.
+	 * @return boolean
+	 */
+	private function doRender( &$error = null ) {
+		$error = null;
+		$host = self::pickHost();
+		$texcmd = urlencode( $this->tex );
+		$post = $this->getLaTeXMLSettings();
+		$post .= '&tex=' . $texcmd;
+		if ( $this->makeRequest( $host, $post, $res, $error ) ) {
+			$result = json_decode( $res );
+			if ( json_last_error() === JSON_ERROR_NONE ) {
+				if ( self::isValidMathML( $result->result ) ) {
+					$this->setMathml( $result->result );
+					return true;
+				} else {
+					// Do not print bad mathml. It's probably too verbose and might
+					// mess up the browser output.
+					$error = $this->getError( 'math_latexml_invalidxml', $host );
+					wfDebugLog( "Math", "\nLaTeXML InvalidMathML:"
+						. var_export( array( 'post' => $post, 'host' => $host
+							, 'result' => $result ), true ) . "\n\n" );
+					return false;
+				}
+			} else {
+					$error = $this->getError( 'math_latexml_invalidjson', $host );
+					wfDebugLog( "Math", "\nLaTeXML InvalidJSON:"
+						. var_export( array( 'post' => $post, 'host' => $host
+							, 'res' => $res ), true ) . "\n\n" );
+					return false;
+				}
+		} else {
+			// Error message has already been set.
+			return false;
+		}
+	}
+
+	/**
+	 * Checks if the input is valid MathML,
+	 * and if the root element has the name math
+	 * @param string $XML
+	 * @return boolean
+	 */
+	static public function isValidMathML( $XML ) {
+		$out = false;
+		$prevInternalErrors = libxml_use_internal_errors( true );
+		$xml = simplexml_load_string( $XML );
+		if ( !$xml ) {
+			wfDebugLog( "Math", "XML validation error:\n " . var_export( $XML, true ) . "\n" );
+			foreach ( libxml_get_errors() as $error ) {
+				wfDebugLog( "Math", "\t" . $error->message );
+			}
+			libxml_clear_errors();
+		} else {
+			$name = $xml->getName();
+			if ( $name == "math" or $name == "table" or $name == "div" ) {
+				$out = true;
+			} else {
+				wfDebugLog( "Math", "got wrong root element " . $name );
+			}
+		}
+		libxml_use_internal_errors( $prevInternalErrors );
+		return $out;
+	}
+
+	/**
+	 * Internal version of @link self::embedMathML
+	 * @return string
+	 * @return html element with rendered math
+	 */
+	private function getMathMLTag() {
+		return self::embedMathML( $this->getMathml()
+			, urldecode( $this->getTex() ) );
+	}
+
+	/**
+	 * Embeds the MathML-XML element in a HTML span element with class tex
+	 * @param string $mml: the MathML string
+	 * @param string $tagId: optional tagID for references like (pagename#equation2)
+	 * @return html element with rendered math
+	 */
+	public static function embedMathML( $mml, $tagId = '', $attribs = false ) {
+		$mml = str_replace( "\n", " ", $mml );
+		if ( ! $attribs ) {
+			$attribs = array( 'class' => 'tex', 'dir' => 'ltr' );
+			if ( $tagId ) {
+				$attribs['id'] = $tagId;
+			}
+			$attribs = Sanitizer::validateTagAttributes( $attribs, 'span' );
+		}
+		return Xml::tags( 'span', $attribs, $mml );
+	}
+
+}
---
+++ tests/MathLaTeXMLTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+* Test the LaTeXML output format.
+*
+* @group Math
+*/
+class MathLaTeXMLTest extends MediaWikiTestCase {
+
+	/**
+	 * Tests behavior of makeRequest() that communicates with the host.
+	 * @covers MathTexvc::makeRequest
+	 */
+	function testMakeRequest() {
+		global $wgLaTeXMLTimeout;
+		// TODO: How to mock HTTP::post()
+		$renderer = MathRenderer::getRenderer( "a+b", array(), MW_MATH_LATEXML );
+
+		$requestReturn = $renderer->makeRequest( 'http://www.google.com'
+			, 'a+b', $res, $error );
+		$this->assertEquals( false, $requestReturn, "requestReturn is false if HTTP::post returns false." );
+		$this->assertEquals( false, $res, "res is false if HTTP:post returns false." );
+		$errmsg = wfMessage( 'latexml_invalidresponse' )->inContentLanguage()->escaped();
+		$this->assertContains($errmsg, $error, "return an error if HTTP::post returns false" );
+
+		$requestReturn = $renderer->makeRequest( 'http://latexml.mathweb.org/convert'
+			, 'a+b', $res, $error );
+		$this->assertEquals( true, $requestReturn, "successful call return" );
+		$this->isTrue( $res, "successfull call" );
+		$this->assertNull( $error, "successfull call errormessage" );
+
+		$this->setMwGlobals( 'wgLaTeXMLTimeout', 1);
+		$requestReturn = $renderer->makeRequest( 'http://latexml.mathweb.org/convert'
+			, '$\renewcommand\foo{\foo}\foo$', $res, $error );
+		$this->assertEquals( false, $requestReturn, "timeout call return" );
+		$this->assertEquals( false, $res, "timeout call return" );
+		$errmsg = wfMessage( 'math_latexml_timeout' )->inContentLanguage()->escaped();
+		$this->assertContains($errmsg, $error, "timeout call errormessage" );
+
+		$this->setMwGlobals( 'wgLaTeXMLTimeout', '.000000001'); //jenkins is very fast or caches $wg* PROBLEM!
+		$requestReturn = $renderer->makeRequest( 'http://latexml.mathweb.org/convert'
+				, '$a+b$', $res, $error );
+		$this->assertEquals( false, $requestReturn, "timeout call return" );
+		$this->assertEquals( false, $res, "timeout call return" );
+		$errmsg = wfMessage( 'math_latexml_timeout' )->inContentLanguage()->escaped();
+		$this->assertContains($errmsg, $error, "timeout call errormessage" );
+	}
+
+	/**
+	 * Checks the basic functionallity
+	 * i.e. if the span element is generated right.
+	 */
+	public function testIntegration() {
+		global $wgLaTeXMLTimeout;
+		$wgLaTeXMLTimeout = 20;
+		$renderer = MathRenderer::getRenderer( "a+b", array(), MW_MATH_LATEXML );
+		$real = $renderer->render( true );
+		$expected = '<span class="tex" dir="ltr" id="a_b"><math xmlns="http://www.w3.org/1998/Math/MathML" id="p1.1.m1" class="ltx_Math" alttext="a+b" xml:id="p1.1.m1.1" display="inline" xref="p1.1.m1.1.cmml">   <semantics xml:id="p1.1.m1.1a" xref="p1.1.m1.1.cmml">     <mrow xml:id="p1.1.m1.1.4" xref="p1.1.m1.1.4.cmml">       <mi xml:id="p1.1.m1.1.1" xref="p1.1.m1.1.1.cmml">a</mi>       <mo xml:id="p1.1.m1.1.2" xref="p1.1.m1.1.2.cmml">+</mo>       <mi xml:id="p1.1.m1.1.3" xref="p1.1.m1.1.3.cmml">b</mi>     </mrow>     <annotation-xml xml:id="p1.1.m1.1.cmml" encoding="MathML-Content" xref="p1.1.m1.1">       <apply xml:id="p1.1.m1.1.4.cmml" xref="p1.1.m1.1.4">         <plus xml:id="p1.1.m1.1.2.cmml" xref="p1.1.m1.1.2"/>         <ci xml:id="p1.1.m1.1.1.cmml" xref="p1.1.m1.1.1">a</ci>         <ci xml:id="p1.1.m1.1.3.cmml" xref="p1.1.m1.1.3">b</ci>       </apply>     </annotation-xml>     <annotation xml:id="p1.1.m1.1b" encoding="application/x-tex" xref="p1.1.m1.1.cmml">a+b</annotation>   </semantics> </math></span>';
+		$this->assertEquals( $expected, $real
+				, "Rendering of a+b in plain Text mode" );
+	}
+}
