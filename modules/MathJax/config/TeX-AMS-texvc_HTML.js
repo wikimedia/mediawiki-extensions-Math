950ed6224a4a51657a6204d92eb9ad4b826b07e4
Derk-Jan Hartman <hartman@videolan.org>
2013-05-19 17:22:58 +0200

    Math: Enable MathJax's Safe mode
    This prevents href and style etc commands to be used
    http://docs.mathjax.org/en/latest/safe-mode.html
    
    href and style commands are already blacklisted in MathJax's texvc config,
    but the commands require, classes and CSSids, were not.
    
    Change-Id: I21d01540e58b8a2fc21d239268be29250e70b6e1

--- modules/MathJax/config/TeX-AMS-texvc_HTML.js
+++ modules/MathJax/config/TeX-AMS-texvc_HTML.js
@@ -1,5 +1,5 @@
 MathJax.Hub.Config({
-  extensions: ["wiki2jax.js","MathEvents.js","MathZoom.js","MathMenu.js","toMathML.js"],
+  extensions: MathJax.Hub.config.extensions.concat("wiki2jax.js","MathEvents.js","MathZoom.js","MathMenu.js","toMathML.js"),
   jax: ["input/TeX","output/HTML-CSS"],
   TeX: {extensions: ["noUndefined.js","AMSmath.js","AMSsymbols.js","texvc.js", "color.js", "cancel.js"]}
 });
