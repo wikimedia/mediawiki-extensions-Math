b032eb09294b73603b2b7ad589d7b07a79226e4c
physikerwelt <wiki@physikerwelt.de>
2013-06-18 09:00:41 +0000

    Enabling MathJaX in LaTeXML rendering mode except for Firefox
    Due to a bad MathML support for all browsers, expect Firefox,
    the LaTeXML rendering mode will use MathJaX's MML to something
    that can be displayed by the browser rendering mode.
    This is not the same as enabling MathJaX since it is much faster
    since the MathML must only be transformed to e.g. SVG. In contrast
    to that MathJaX must compile the TeX code in order to get the SVG
    output at the client side which takes much longer.
    
    Change-Id: I6ad17a04e9d4afff71ebbb3f4bc3cb6d4fd15e5d

---
+++ modules/ext.math.mathjax.enabler.mml.js
@@ -0,0 +1,88 @@
+/**
+ * From https://en.wikipedia.org/wiki/User:Nageh/mathJax.js
+ */
+/*global mathJax:true, MathJax */
+( function ( mw, $ ) {
+  if ( typeof mathJax === 'undefined' ) {
+    mathJax = {};
+  }
+
+  mathJax.version = '0.2';
+
+  mathJax.loaded = false;
+
+  mathJax.config = $.extend( true, {
+    root: mw.config.get('wgExtensionAssetsPath') + '/Math/modules/MathJax',
+    config: ['MML_HTMLorMML-full.js'],
+    'v1.0-compatible': false,
+    displayAlign: 'left',
+    menuSettings: {
+      zoom: 'Click'
+    },
+    'HTML-CSS': {
+      imageFont: null,
+      availableFonts: ['TeX'],
+      mtextFontInherit: true
+    }
+  }, mathJax.config );
+
+  mathJax.Config = function () {
+    MathJax.Hub.Config( mathJax.config );
+    MathJax.OutputJax.fontDir = mw.config.get('wgExtensionAssetsPath') + '/Math/modules/MathJax/fonts';
+  };
+
+  /**
+   * Renders all Math TeX inside the given elements.
+   * @param {function} callback to be executed after text elements have rendered [optional]
+   */
+  $.fn.renderTex = function ( callback ) {
+    var elem = this.find( '.tex' ).parent().toArray();
+
+    if ( !$.isFunction( callback ) ) {
+      callback = $.noop;
+    }
+
+    function render () {
+      MathJax.Hub.Queue( ['Typeset', MathJax.Hub, elem, callback] );
+    }
+
+    mw.loader.using( 'ext.math.mathjax', function () {
+      if ( MathJax.isReady ) {
+        render();
+      } else {
+        MathJax.Hub.Startup.signal.MessageHook( 'End', render );
+      }
+    });
+    return this;
+  };
+
+  mathJax.Load = function () {
+    var config, script;
+    if (this.loaded) {
+      return true;
+    }
+
+    // create configuration element
+    config = 'mathJax.Config();';
+    script = document.createElement( 'script' );
+    script.setAttribute( 'type', 'text/x-mathjax-config' );
+    if ( window.opera ) {
+      script.innerHTML = config;
+    } else {
+      script.text = config;
+    }
+    document.getElementsByTagName('head')[0].appendChild( script );
+
+    // create startup element
+    mw.loader.load('ext.math.mathjax');
+
+    this.loaded = true;
+
+    return false;
+  };
+
+  $( document ).ready( function () {
+    mathJax.Load();
+  } );
+
+}( mediaWiki, jQuery ) );
