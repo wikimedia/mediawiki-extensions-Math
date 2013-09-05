fa20a6b55bc0be2e717124cf019f752717dc5186
Frédéric Wang <fred.wang@free.fr>
2013-09-05 15:01:21 +0200

    MathJax 2.3 upgrade and loading improvement
    WORK IN PROGRESS: DO NOT SUBMIT!
    
    - upgrade to MathJax 2.3 and use the unpacked version.
    - use MediaWiki's resource loader.
    - improve the configuration.
    
    Change-Id: I365367d6df00c482bed5c6b9759eece12aff7d47

---
+++ modules/MathJax/unpacked/localization/de/MathMenu.js
@@ -0,0 +1,105 @@
+/* -*- Mode: Javascript; indent-tabs-mode:nil; js-indent-level: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+
+/*************************************************************
+ *
+ *  MathJax/localization/de/MathMenu.js
+ *
+ *  Copyright (c) 2009-2013 The MathJax Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+MathJax.Localization.addTranslation("de","MathMenu",{
+        version: "2.3",
+        isLoaded: true,
+        strings: {
+          Show: "Zeige Mathe als",
+          MathMLcode: "MathML Code",
+          OriginalMathML: "Original MathML",
+          TeXCommands: "Original TeX",
+          AsciiMathInput: "Original AsciiMathML",
+          Original: "Originalform",
+          ErrorMessage: "Fehlermeldung",
+          Annotation: "Annotation",
+          TeX: "TeX",
+          StarMath: "StarMath",
+          Maple: "Maple",
+          ContentMathML: "Content MathML",
+          OpenMath: "OpenMath",
+          texHints: "TeX Tipps in MathML",
+          Settings: "Einstellungen",
+          ZoomTrigger: "Zoom ausl\u00F6sen",
+          Hover: "Hover",
+          Click: "Klick",
+          DoubleClick: "Doppelklick",
+          NoZoom: "Kein Zoom",
+          TriggerRequires: "Ausl\u00F6ser ben\u00F6tigt:",
+          Option: "Option",
+          Alt: "Alt",
+          Command: "Command",
+          Control: "Steuerung",
+          Shift: "Shift",
+          ZoomFactor: "Zoomfaktor",
+          Renderer: "Mathe Renderer",
+          MPHandles: "An MathPlayer \u00FCbergeben:",
+          MenuEvents: "Men\u00FC Events",
+          MouseEvents: "Maus Events",
+          MenuAndMouse: "Maus und Men\u00FC Events",
+          FontPrefs: "Font Einstellungen",
+          ForHTMLCSS: "F\u00FCr HTML-CSS",
+          Auto: "Auto",
+          TeXLocal: "TeX (lokal)",
+          TeXWeb: "TeX (Web)",
+          TeXImage: "TeX (Bild)",
+          STIXLocal: "STIX (lokal)",
+          ContextMenu: "Kontextmen\u00FC ",
+          Browser: "Browser",
+          Scale: "Alle Mathe skalieren ...",
+          Discoverable: "Highlight durch Hovern",
+          Locale: "Sprache",
+          LoadLocale: "Von URL laden ...",
+          About: "\u00DCber MathJax",
+          Help: "MathJax Hilfe",
+          localTeXfonts: "Lokale TeX-Fonts werden verwendet",
+          webTeXfonts: "TeX-Webfonts werden verwendet",
+          imagefonts: "Bild-Fonts werden verwendet",
+          localSTIXfonts: "Lokale STIX-Fonts vverwendet",
+          webSVGfonts: "SVG-Webfonts werden verwendet",
+          genericfonts: "Generische Unicode-Fonts werden verwendet",
+          wofforotffonts: "WOFF- oder OTF-Fonts",
+          eotffonts: "EOT-Fonts",
+          svgfonts: "SVG-Fonts",
+          WebkitNativeMMLWarning: "Ihr Browser scheint MathML nicht zu unterst\u00FCtzen, so dass ein Wechsel zur MathML-Ausgabe die Mathematik auf der Seite unlesbar machen k\u00F6nnte.",
+          MSIENativeMMLWarning: "Internet Explorer ben\u00F6tigt das MathPlayer Plugin, um MathML-Ausgabe darstellen zu k\u00F6nnen.",
+          OperaNativeMMLWarning: "Opera's MathML Unterst\u00FCtzung ist beschr\u00E4nkt, so dass beim Wechsel zur MathML-Ausgabe einige Ausdr\u00FCcke schlecht gerendert werden.",
+          SafariNativeMMLWarning: "Die MathML-Unterst\u00FCtzung Ihres Browsers beherrscht nicht alle MathJax-Features, so dass einige Ausdr\u00FCcke schlecht gerendert werden.",
+          FirefoxNativeMMLWarning: "Die MathML-Unterst\u00FCtzung Ihres Browsers beherrscht nicht alle MathJax-Features, so dass einige Ausdr\u00FCcke schlecht gerendert werden.",
+          MSIESVGWarning: "Internet Explorer unterst\u00FCtzt SVG erst ab IE9 und nicht im IE8-Emulationsmodus. Beim Wechsel zur SVG-Ausgabe wird die Mathematik nicht richtig dargestellt.",
+          LoadURL: "Sprachschema von URL laden:",
+          BadURL: "URL muss eine JavaScript-Datei f\u00FCr MathJax Sprachschema verlinken. JavaScript Dateinamen sollten auf '.js' enden.",
+          BadData: "Fehler beim Laden des Sprachschema von %1",
+          SwitchAnyway: "Renderer trotzdem \u00E4ndern?\n\n(Mit OK wechseln, mit ABBRECHEN den aktuellen Renderer verwenden)",
+          ScaleMath: "Alle Mathematik skalieren (relativ zum umgebenden Text)",
+          NonZeroScale: "Skalierung darf nicht Null sein",
+          PercentScale: "Skalierung muss in Prozent sein (z.B. 120%%)",
+          IE8warning: "Dies Deaktiviert das MathJax Men\u00FC und den MathJax Zoom. Alt+Klick auf eine Formel zeigt weiter das MathJax-Men\u00FC.\n\nWirklich MathPlayer Einstellungen \u00E4ndern?",
+          IE9warning: "Das MathJax Men\u00FC wird deaktiviert und kann nur durch Alt+Klick auf eine Formel angezeigt werden.",
+          NoOriginalForm: "Keine Originalform verf\u00FCgbar",
+          Close: "Schliessen",
+          EqSource: "Original MathJax Formel"
+        }
+});
+
+MathJax.Ajax.loadComplete("[MathJax]/localization/de/MathMenu.js");
