08eb5738975700cbbe4eec8315907ab1759755fd
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2013-09-23 17:41:09 -0700

    Math 2.0
    * MathML support with SVG fallback
    * Distinction between inline and display-style
    * MathJax now available independent of the rendering mode
    * Debug functionality
    * independent of the file-system
    * backwards compatible, without dependencies to it
    * removes dependencies to math specific core functions
    ** getMathOptions()
    ** armourMath
    * Asynchronous generation of Math images
    * upgrade to MathJax 2.3, use the unpacked version and remove unusued files
    * use MediaWiki's resource loader for Javascript files
    * improve the way MathJax is configured
    * make math fonts available as Web fonts for native MathML
    * The MathML/SVG output is used as a preview while MathJax is processing
    * new table (mathoid) enables quick migration from Math1.0 to Math 2.0
     (no schema change for math table required)
    
    MathML is currently displayed for Firefox and MathPlayer users only.
    By default it is wrapped in a hidden span or div element.
    
    SVG generation is done by using MathJax on the server-side.
    
    (See a live demo http://math-test2.instance-proxy.wmflabs.org/wiki/Fourier_series)
    
    PS31: Adresses the displaystyle concern of Frédéric (http://math-test2.instance-proxy.wmflabs.org/wiki/Displaystyle)
    and the ID feature request of Gabriel Wicke (http://math-test2.instance-proxy.wmflabs.org/wiki/IDs#eq.20-sim)
    
    PS35: Add extra cache keys.
    
    PS49: Edit merge conflicts with I9066f6b2606044412a9b91ca395e841550d7e787
    
    Change-Id: I5132901ed2072c6af7b9ed82d267200d64c67939

---
+++ MathInputCheckTexvc.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * MediaWiki math extension
+ *
+ * (c) 2002-2013 Tomasz Wegrzanowski, Brion Vibber, Moritz Schubotz, and other MediaWiki contributors
+ * GPLv2 license; info in main package.
+ *
+ * @author Moritz Schubotz
+ */
+class MathInputCheckTexvc extends MathInputCheck {
+
+	/**
+	 * Converts an error returned by texvc to a localized exception
+	 *
+	 * @param string $texvcResult error result returned by texvc
+	 */
+	public function convertTexvcError($texvcResult) {
+		$texvcStatus = substr($texvcResult, 0, 1);
+		$errorRenderer = new MathSource($this->inputTeX);
+		$errDetails = htmlspecialchars(substr($texvcResult, 1));
+		switch ($texvcStatus) {
+			case 'E':
+				$errMsg = $errorRenderer->getError('math_lexing_error');
+				break;
+			case 'S':
+				$errMsg = $errorRenderer->getError('math_syntax_error');
+				break;
+			case 'F':
+				$errMsg = $errorRenderer->getError('math_unknown_function', $errDetails);
+				break;
+			default:
+				$errMsg = $errorRenderer->getError('math_unknown_error');
+		}
+
+		return $errMsg;
+	}
+
+	/**
+	 *
+	 * @global type $wgTexvc
+	 * @return boolean
+	 */
+	public function isValid() {
+		global $wgMathTexvcCheckExecutable;
+		if (!is_executable($wgMathTexvcCheckExecutable)) {
+			$errorRenderer = new MathSource($this->inputTeX);
+			$this->lastError = $errorRenderer->getError('math_notexvc');
+			return false;
+		}
+
+		$cmd = $wgMathTexvcCheckExecutable . ' ' . wfEscapeShellArg($this->inputTeX);
+
+		if (wfIsWindows()) {
+			# Invoke it within cygwin sh, because texvc expects sh features in its default shell
+			$cmd = 'sh -c ' . wfEscapeShellArg($cmd);
+		}
+		wfDebugLog('Math', "TeX check command: $cmd\n");
+		$contents = wfShellExec($cmd);
+		wfDebugLog('Math', "TeX check result:\n $contents\n---\n");
+
+		if (strlen($contents) == 0) {
+			wfDebugLog('Math', "TeX check output was empty. \n");
+			$this->lastError = MathRenderer::getError('math_unknown_error');
+			return false;
+		}
+
+		$retval = substr($contents, 0, 1);
+		if ($retval != "+") {
+			$this->lastError = $this->convertTexvcError($contents);
+			wfDebugLog('Math', 'checkTex failed:' . $this->lastError);
+			return false;
+		} else {
+			$this->validTeX = substr($contents, 1);
+			$this->isSecure = true;
+			wfDebugLog('Math', 'checkTex successful tex is now: ' . $this->validTeX);
+			return true;
+		}
+	}
+
+}
