69d6e11a7a878fe6bb6d090854fc1db31ed247d2
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2013-09-23 17:41:09 -0700

    Backup
    ...testing and reviewing...
    ... all tests work locally...
    .. mathjax is not ready to be released ..
    .. jenkins can't add fields to the database..
    .. further testing and debugging ...
    Change-Id: I5132901ed2072c6af7b9ed82d267200d64c67939

---
+++ Math.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for Math
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'MathShowImage' => array( 'MathShowImage', 'MathShowImage' )
+);
