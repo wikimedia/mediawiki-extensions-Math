40a1fdd5f315d900ec20392d3d78913a9c725842
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2013-09-23 17:41:09 -0700

    Math 2.0
    * MathML support with SVG fallback
    * Distinction between inline and display-style
    * MathJax now available independent of the rendering mode
    * Debug functionality
    * independent of the file-system
    * backwards compatible, without dependencies to it
    * removes dependencies to math specific core functions
    ** getMathOptions()
    ** armourMath
    * Asynchronous generation of Math images
    * upgrade to MathJax 2.3, use the unpacked version and remove unusued files
    * use MediaWiki's resource loader for Javascript files
    * improve the way MathJax is configured
    * make math fonts available as Web fonts for native MathML
    * The MathML/SVG output is used as a preview while MathJax is processing
    * new table (mathoid) enables quick migration from Math1.0 to Math 2.0
     (no schema change for math table required)
    
    MathML is currently displayed for Firefox and MathPlayer users only.
    By default it is wrapped in a hidden span or div element.
    
    SVG generation is done by using MathJax on the server-side.
    
    (See a live demo http://math-test2.instance-proxy.wmflabs.org/wiki/Fourier_series)
    
    PS31: Adresses the displaystyle concern of Frédéric (http://math-test2.instance-proxy.wmflabs.org/wiki/Displaystyle)
    and the ID feature request of Gabriel Wicke (http://math-test2.instance-proxy.wmflabs.org/wiki/IDs#eq.20-sim)
    
    PS35: Add extra cache keys.
    
    PS49: Edit merge conflicts with I9066f6b2606044412a9b91ca395e841550d7e787
    
    Change-Id: I5132901ed2072c6af7b9ed82d267200d64c67939

--- Math.hooks.php
+++ Math.hooks.php
@@ -2,26 +2,25 @@
 /**
  * MediaWiki math extension
  *
- * (c) 2002-2012 various MediaWiki contributors
+ * (c) 2002-2013 various MediaWiki contributors
  * GPLv2 license; info in main package.
  */
 
 class MathHooks {
-	/**
-	 * Set up $wgMathPath and $wgMathDirectory globals if they're not already
-	 * set.
+	/*
+	 *
+	 * @param &$confstr The to-be-hashed key string that is being constructed
 	 */
-	static function setup() {
-		global $wgMathPath, $wgMathDirectory;
-		global $wgUploadPath, $wgUploadDirectory;
-		if ( $wgMathPath === false ) {
-			$wgMathPath = "{$wgUploadPath}/math";
+	public static function onPageRenderingHash( &$confstr ) {
+		global $wgUser,$wgMathJax;
+		$user = $wgUser;
+		$confstr .= "!" . $user->getOption('math');
+		if ( $wgMathJax &&  $user->getOption('mathJax') ){
+			$confstr .= "!" . 1;
 		}
-		if ( $wgMathDirectory === false ) {
-			$wgMathDirectory = "{$wgUploadDirectory}/math";
-		}
+		wfDebugLog('Math', 'New cache key'.$confstr);
+		return true;
 	}
-
 	/**
 	 * Register the <math> tag with the Parser.
 	 *
@@ -36,29 +35,55 @@ class MathHooks {
 	/**
 	 * Callback function for the <math> parser hook.
 	 *
-	 * @param $content (the LaTeX input)
+	 * @param $content the LaTeX input
 	 * @param $attributes
-	 * @param $parser Parser
+	 * @param Parser $parser
 	 * @return string
 	 */
 	static function mathTagHook( $content, $attributes, $parser ) {
-		global $wgContLang, $wgUseMathJax;
-		if ( trim( $content )  === "" ) { // bug 8372
+		global $wgMathJax, $wgMathDisableTexFilter;
+		if ( trim( $content ) === "" ) { // bug 8372
 			return "";
 		}
 		wfProfileIn( __METHOD__ );
-		$mode = $parser->getOptions()->getMath();
+		$mode = (int) $parser->getUser()->getOption('math');
+		$parser->getOptions()->addExtraKey($mode);
 		$renderer = MathRenderer::getRenderer(
 			$content, $attributes, $mode
 		);
-		$renderedMath = $renderer->render();
-		if ( $wgUseMathJax && $mode == MW_MATH_MATHJAX ) {
+		if (! $wgMathDisableTexFilter){
+			$checkResult = $renderer->checkTex();
+			if ( $checkResult !== true ){
+				//returns the error message
+				return $renderer->getLastError();
+			}
+		}
+		if ( $renderer->render() ){
+			wfDebugLog( "Math" , "Rendering successfull. Writing output");
+			$renderedMath = $renderer->getHtmlOutput();
+		} else {
+			wfDebugLog( "Math" , "Rendering failed. Prining error message.");
+			return $renderer->getLastError();
+		}
+		wfRunHooks( 'MathFormulaRendered',
+			array( &$renderer,
+				&$renderedMath,
+				$parser->getTitle()->getArticleID(),
+				$parser->nextLinkID()) );//Enables indexing of math formula
+		if ( $wgMathJax && $parser->getUser()->getOption('mathJax')) {
+			//maybe this can be checked in the javascript, this would be better for caching
+			$parser->getOptions()->addExtraKey(1);
 			$parser->getOutput()->addModules( array( 'ext.math.mathjax.enabler' ) );
 		}
+		$parser->getOutput()->addModuleStyles( array( 'ext.math.styles' ) );
+		$parser->getOutput()->addModuleStyles( array( 'ext.math.styles.fonts' ) );
+		//$parser->getOutput()->addModules( array( 'ext.math.selector' ) );
+
+		// Writes cache if rendering was successful
 		$renderer->writeCache();
-		$result = $wgContLang->armourMath( $renderedMath );
+		//Check if we needed that MathRenderer::armourMath( $renderedMath);
 		wfProfileOut( __METHOD__ );
-		return $result;
+		return array($renderedMath, "markerType" => 'nowiki' );
 	}
 
 	/**
@@ -69,12 +94,20 @@ class MathHooks {
 	 * @return Boolean: true
 	 */
 	static function onGetPreferences( $user, &$defaultPreferences ) {
+		global $wgMathJax;
 		$defaultPreferences['math'] = array(
 			'type' => 'radio',
 			'options' => array_flip( self::getMathNames() ),
 			'label' => '&#160;',
 			'section' => 'rendering/math',
 		);
+		if ( $wgMathJax ) {
+			$defaultPreferences['mathJax'] = array(
+				'type' => 'toggle',
+				'label-message' => 'mw_math_mathjax',
+				'section' => 'rendering/math',
+			);
+		}
 		return true;
 	}
 
@@ -84,16 +117,16 @@ class MathHooks {
 	 * @return array of strings
 	 */
 	private static function getMathNames() {
-		global $wgUseMathJax, $wgUseLaTeXML;
-		$names = array(
-			MW_MATH_PNG => wfMessage( 'mw_math_png' )->escaped(),
-			MW_MATH_SOURCE => wfMessage( 'mw_math_source' )->escaped(),
+		global $wgMathValidModes;
+		$MathConstantNames = array(
+			MW_MATH_SOURCE => 'mw_math_source',
+			MW_MATH_PNG => 'mw_math_png',
+			MW_MATH_MATHML => 'mw_math_mathml',
+			MW_MATH_LATEXML => 'mw_math_latexml'
 		);
-		if ( $wgUseMathJax ) {
-			$names[MW_MATH_MATHJAX] = wfMessage( 'mw_math_mathjax' )->escaped();
-		}
-		if ( $wgUseLaTeXML ) {
-			$names[MW_MATH_LATEXML] = wfMessage( 'mw_math_latexml' )->escaped();
+		$names=array();
+		foreach ($wgMathValidModes as $mode) {
+			$names[$mode] = wfMessage( $MathConstantNames[$mode] )->escaped();
 		}
 		return $names;
 	}
@@ -108,7 +141,9 @@ class MathHooks {
 		global $wgUser;
 
 		# Don't generate TeX PNGs (lack of a sensible current directory causes errors anyway)
+		//TODO: revalidate that
 		$wgUser->setOption( 'math', MW_MATH_SOURCE );
+
 		return true;
 	}
 
@@ -120,22 +155,33 @@ class MathHooks {
 	 * @return bool
 	 */
 	static function onLoadExtensionSchemaUpdates( $updater = null ) {
+		global $wgMathDebug;
 		if ( is_null( $updater ) ) {
 			throw new MWException( "Math extension is only necessary in 1.18 or above" );
 		}
-		$map = array(
-			'mysql' => 'math.sql',
-			'sqlite' => 'math.sql',
-			'postgres' => 'math.pg.sql',
-			'oracle' => 'math.oracle.sql',
-			'mssql' => 'math.mssql.sql',
-		);
+		$map = array( 'mysql', 'sqlite', 'postgres', 'oracle', 'mssql');
 		$type = $updater->getDB()->getType();
-		if ( isset( $map[$type] ) ) {
-			$sql = dirname( __FILE__ ) . '/db/' . $map[$type];
+		if ( $type == 'sqlite' ){
+			$type = 'mysql'; //The commands used from the updater are the same
+		}
+		if (in_array( $type, $map ) ){
+			$sql = dirname( __FILE__ ) . '/db/math.' . $type .'.sql';
 			$updater->addExtensionTable( 'math', $sql );
+			$sql = dirname( __FILE__ ) . '/db/mathoid.' . $type .'.sql';
+			$updater->addExtensionTable( 'mathoid', $sql );
 		} else {
-			throw new MWException( "Math extension does not currently support $type database." );
+			throw new MWException( "Math extension does not currently support $type database.\n" );
+		}
+		if ($wgMathDebug){
+			if($type =='mysql' ){
+				$dir = dirname( __FILE__ ) . '/db/debug_fields_';
+				$updater->addExtensionField('mathoid', 'math_status', $dir.'math_status.sql');
+				$updater->addExtensionField('mathoid', 'math_timestamp', $dir.'math_timestamp.sql');
+				$updater->addExtensionField('mathoid', 'math_log', $dir.'math_log.sql');
+			} else {
+				throw new MWException( "Math extension does not currently support $type database for debugging.\n"
+					. 'Please set $wgMathDebug = false; in your LocalSettings.php' );
+			}
 		}
 		return true;
 	}
@@ -149,20 +195,7 @@ class MathHooks {
 	 */
 	static function onParserTestTables( &$tables ) {
 		$tables[] = 'math';
-		return true;
-	}
-
-	/**
-	 * Hack to fake a default $wgMathPath value so parser test output
-	 * that renders to images doesn't vary by who runs it.
-	 *
-	 * @global string $wgMathPath
-	 * @param Parser $parser
-	 * @return bool
-	 */
-	static function onParserTestParser( &$parser ) {
-		global $wgMathPath;
-		$wgMathPath = '/images/math';
+		$tables[] = 'mathoid';
 		return true;
 	}
 
@@ -176,5 +209,36 @@ class MathHooks {
 		$testDir = __DIR__ . '/tests/';
 		$files = array_merge( $files, glob( "$testDir/*Test.php" ) );
 		return true;
+	}
+
+	/**
+	 * Hack to fake a default $wgMathPath value so parser test output
+	 * that renders to images doesn't vary by who runs it.
+	 *
+	 * @global string $wgMathPath
+	 * @param Parser $parser
+	 * @return bool
+	 * @deprecated since version 2.0
+	 */
+	static function onParserTestParser( &$parser ) {
+		global $wgMathPath;
+		$wgMathPath = '/images/math';
+		return true;
+	}
+
+	/**
+	 * Set up $wgMathPath and $wgMathDirectory globals if they're not already
+	 * set.
+	 * @deprecated since version 2.0
+	 */
+	static function setup() {
+		global $wgMathPath, $wgMathDirectory;
+		global $wgUploadPath, $wgUploadDirectory;
+		if ( $wgMathPath === false ) {
+			$wgMathPath = "{$wgUploadPath}/math";
+		}
+		if ( $wgMathDirectory === false ) {
+			$wgMathDirectory = "{$wgUploadDirectory}/math";
+		}
 	}
 }
--- modules/ext.math.mathjax.enabler.js
+++ modules/ext.math.mathjax.enabler.js
@@ -1,7 +1,7 @@
 /**
  * From https://en.wikipedia.org/wiki/User:Nageh/mathJax.js
  */
-/*global mathJax:true, MathJax */
+/*global mathJax:true, MathJax:true */
 ( function ( mw, $ ) {
   if ( typeof mathJax === 'undefined' ) {
     mathJax = {};
@@ -11,13 +11,13 @@
 
   mathJax.loaded = false;
 
+  mathJax.locale = mw.config.get('wgUserLanguage');
+
+  // See http://docs.mathjax.org/en/latest/options/index.html
   mathJax.config = $.extend( true, {
-    root: mw.config.get('wgExtensionAssetsPath') + '/Math/modules/MathJax',
-    config: ['TeX-AMS-texvc_HTML.js'],
+    root: mw.config.get('wgExtensionAssetsPath') + '/Math/modules/MathJax/unpacked',
     'v1.0-compatible': false,
-    displayAlign: 'left',
     menuSettings: {
-      locale: mw.config.get('wgUserLanguage'),
       zoom: 'Click'
     },
     'HTML-CSS': {
@@ -27,12 +27,173 @@
     },
     MathMenu: {
       showLocale: false
-    }
+    },
+    jax: ['input/TeX','input/MathML','output/NativeMML','output/HTML-CSS']
   }, mathJax.config );
 
-  mathJax.Config = function () {
+  /**
+   * @param {string} relative path to a MathJax file
+   * @return {string} MediaWiki module containing the file
+   */
+  mathJax.getModuleNameFromFile = function (file) {
+    var regexp, module;
+
+    // These modules are loaded at startup and thus don't need to be specified:
+    // - ext.math.mathjax.mathjax in mathJax.Load
+    // - ext.math.mathjax.localization in mathJax.Init
+    // - ext.math.mathjax.jax.config in MathJax.Hub.Startup.Jax
+    // - ext.math.mathjax.extensions in MathJax.Hub.Startup.Extensions
+    //
+
+    module = 'ext.math.mathjax.';
+
+    regexp = file.match(/(.*)\/jax\.js/);
+    if ( regexp ) {
+      // These are jax.js files of input, element or output modules:
+      // - ext.math.mathjax.jax.input.MathML
+      // - ext.math.mathjax.jax.input.TeX
+      // - ext.math.mathjax.jax.output.HTML-CSS
+      // - ext.math.mathjax.jax.output.NativeMML
+      // - ext.math.mathjax.jax.output.HTML-CSS
+      // - ext.math.mathjax.jax.output.SVG
+      return module + regexp[1].replace(/\//g,'.');
+    }
+
+    if ( file.match(/jax\/element\/mml\/optable/) ) {
+      return module + 'jax.element.mml.optable';
+    }
+
+    regexp = file.match(/jax\/output\/(HTML-CSS|SVG)/);
+    if ( regexp ) {
+      module += 'jax.output.' + regexp[1] + '.';
+      if ( file.match(/autoload/) ) {
+        return module + 'autoload';
+      }
+      if ( file.match(/fonts\/TeX\/fontdata/) ) {
+        return module + 'fonts.TeX.fontdata';
+      }
+      if ( file.match(/fonts\/TeX\/.*\/.*\/Main\.js/) ) {
+        return module + 'fonts.TeX.MainJS';
+      }
+      if ( file.match(/fonts\/TeX\/Main/) ) {
+        return module + 'fonts.TeX.Main';
+      }
+      if ( file.match(/fonts\/TeX\/AMS/) ) {
+        return module + 'fonts.TeX.AMS';
+      }
+      if ( file.match(/fonts\/TeX/) ) {
+        return module + 'fonts.TeX.Extra';
+      }
+    }
+
+    mw.log( 'MathJax resource not handled by MediaWiki: ' + file );
+
+    return null;
+  };
+
+  /**
+   * Configure MathJax, preload some files and replace MathJax's resource loader
+   * by our own resource loader.
+   */
+  mathJax.Init = function () {
+    // Configure MathJax
     MathJax.Hub.Config( mathJax.config );
     MathJax.OutputJax.fontDir = mw.config.get('wgExtensionAssetsPath') + '/Math/modules/MathJax/fonts';
+
+    // Redefine MathJax.Hub.Startup.Jax
+    MathJax.Hub.Startup.Jax = function () {
+      var config, jax, i, k, name, queue, callback;
+      //  Save the order of the output jax since they are loading asynchronously
+      config = MathJax.Hub.config;
+      jax = MathJax.Hub.outputJax;
+      for ( i = 0, k = 0; i < config.jax.length; i++ ) {
+        name = config.jax[i].substr(7);
+        if ( config.jax[i].substr(0,7) === 'output/' && jax.order[name] === null ) {
+            jax.order[name] = k;
+            k++;
+        }
+      }
+      queue = MathJax.Callback.Queue();
+      callback = MathJax.Callback({});
+      return queue.Push(
+        ['Post', MathJax.Hub.Startup.signal, 'Begin Jax'],
+        ['using', mw.loader, 'ext.math.mathjax.jax.config', callback],
+        callback,
+        ['Post', MathJax.Hub.Startup.signal, 'End Jax']
+      );
+    };
+
+    // Redefine MathJax.Hub.Startup.Extensions
+    MathJax.Hub.Startup.Extensions = function () {
+      var queue, callback;
+      queue = MathJax.Callback.Queue();
+      callback = MathJax.Callback({});
+      return queue.Push(
+        ['Post', MathJax.Hub.Startup.signal, 'Begin Extensions'],
+        ['using', mw.loader, 'ext.math.mathjax.extensions', callback],
+        callback,
+        ['Post', MathJax.Hub.Startup.signal, 'End Extensions']
+      );
+    };
+
+    // Redefine MathJax.Ajax.Load
+    MathJax.Ajax.MathJaxLoad = MathJax.Ajax.Load;
+    MathJax.Ajax.Load = function (file, callback) {
+      var type, i, module;
+      callback = MathJax.Callback(callback);
+      if ( file instanceof Object ) {
+        for ( i in file ) {
+          if ( file.hasOwnProperty(i) ) {
+            type = i.toUpperCase();
+            file = file[i];
+          }
+        }
+      } else {
+        type = file.split(/\./).pop().toUpperCase();
+      }
+      file = MathJax.Ajax.fileURL(file);
+      if ( MathJax.Ajax.loading[file] ) {
+        MathJax.Ajax.addHook(file, callback);
+      } else {
+        if ( MathJax.Ajax.loader[type] ) {
+          module = mathJax.getModuleNameFromFile(file.substring(MathJax.Hub.config.root.length + 1));
+          if ( module ) {
+            // Use MediaWiki's resource loader.
+            MathJax.Ajax.loading[file] = {
+              callback: callback,
+              timeout: -1,
+              status: this.STATUS.OK,
+              script: null
+            };
+            // Add this to the structure above after it is created to prevent
+            // recursion when loading the initial localization file (before
+            // loading message is available)
+            MathJax.Ajax.loading[file].message = MathJax.Message.File(file);
+            mw.loader.load(module);
+          } else {
+            // Fallback to MathJax's own loader.
+            callback = MathJax.Ajax.MathJaxLoad(file, callback);
+          }
+        } else {
+          throw new Error('Can\'t load files of type ' + type);
+        }
+      }
+      return callback;
+    };
+
+    // Register the final action after MathJax processing: hide
+    // SVG/PNG preview if thas was not done before and show MathJax's output.
+    MathJax.Hub.Startup.signal.MessageHook( 'End', function () {
+      mw.loader.load('ext.math.mathml');
+    });
+
+    // Set MathJax's locale and load the localization data.
+    MathJax.Localization.resetLocale(mathJax.locale);
+    mathJax.locale = MathJax.Localization.locale;
+    MathJax.Hub.config.menuSettings.locale = mathJax.locale;
+    mw.loader.using( 'ext.math.mathjax.localization', function () {
+      MathJax.Hub.Configured();
+    } );
   };
 
   /**
@@ -50,35 +211,29 @@
       MathJax.Hub.Queue( ['Typeset', MathJax.Hub, elem, callback] );
     }
 
-    mw.loader.using( 'ext.math.mathjax', function () {
+    mw.loader.using( 'ext.math.mathjax.mathjax', function () {
       if ( MathJax.isReady ) {
         render();
       } else {
         MathJax.Hub.Startup.signal.MessageHook( 'End', render );
       }
-    });
+    } );
     return this;
   };
 
   mathJax.Load = function () {
-    var config, script;
-    if (this.loaded) {
+    if ( this.loaded ) {
       return true;
     }
 
-    // create configuration element
-    config = 'mathJax.Config();';
-    script = document.createElement( 'script' );
-    script.setAttribute( 'type', 'text/x-mathjax-config' );
-    if ( window.opera ) {
-      script.innerHTML = config;
-    } else {
-      script.text = config;
-    }
-    document.getElementsByTagName('head')[0].appendChild( script );
+    // create the global MathJax variable to hook into MathJax startup
+    MathJax = {
+      delayStartupUntil: 'configured',
+      AuthorInit: mathJax.Init
+    };
 
-    // create startup element
-    mw.loader.load('ext.math.mathjax');
+    // load MathJax.js
+    mw.loader.load('ext.math.mathjax.mathjax');
 
     this.loaded = true;
 
