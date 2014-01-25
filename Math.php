96dd0a68e1811e4211a6ccd757fda93e2bee89f7
Moritz Schubotz <wiki@physikerwelt.de>
2014-01-26 00:09:06 +0100

    WIP: Editbutton for math
    Imported button image from:
    ssh://gerrit.wikimedia.org:29418/mediawiki/core.git/skins/common/button_math.png
    
    We don't need to vary the icon per language, because there is not a
    single language that has a non default icon.
    
    AND PLEASE DO NOT SUBMIT.
    Bug: 60471
    Change-Id: I3d126c1c252c27fa65ab8fb42ffab66a2d99cfa8

--- Math.php
+++ Math.php
@@ -153,6 +153,7 @@ $wgHooks['LoadExtensionSchemaUpdates'][] = 'MathHooks::onLoadExtensionSchemaUpda
 $wgHooks['ParserTestTables'][] = 'MathHooks::onParserTestTables';
 $wgHooks['UnitTestsList'][] = 'MathHooks::onRegisterUnitTests';
 $wgHooks['PageRenderingHash'][] = 'MathHooks::onPageRenderingHash';
+$wgHooks['EditPageBeforeEditToolbar'][] = 'MathHooks::onEditPageBeforeEditToolbar';
 
 $dir = __DIR__ . '/';
 $wgAutoloadClasses['MathHooks'] = $dir . 'Math.hooks.php';
@@ -403,6 +404,16 @@ $moduleTemplate = array(
     'remoteExtPath' => 'Math/modules',
 );
 
+$wgResourceModules['ext.math.editbutton.enabler'] = array(
+	'localBasePath' => dirname( __FILE__ ) . '/modules/',
+	'remoteExtPath' => 'Math/modules',
+	'scripts' => 'ext.math.editbutton.js',
+	'messages' => array(
+		'math_tip',
+		'math_sample',
+	),
+);
+
 $wgResourceModules['ext.math.visualEditor'] = array(
 	'scripts' => array(
 		'VisualEditor/ve.dm.MWMathNode.js',
@@ -420,3 +431,4 @@ $wgResourceModules['ext.math.visualEditor'] = array(
 ) + $moduleTemplate;
 
 $wgVisualEditorPreferenceModules['math-enable-visualeditor'] = 'ext.math.visualEditor';
+
