2129ef952fc98a76b8cbf43fc8b1e8811ffeebaf
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2014-06-03 10:15:53 +0200

    WIP: Wait until previous commit is merged...
    Bug: 65973
    
    Change-Id: I50517ba83f9a0d2aa8e237f062f18e4319ddbac8

--- tests/MathLaTeXMLTest.php
+++ tests/MathLaTeXMLTest.php
@@ -6,89 +6,7 @@
 */
 class MathLaTeXMLTest extends MediaWikiTestCase {
 
-	// State-variables for HTTP Mockup classes
-	public static $content = null;
-	public static $good = false;
-	public static $html = false;
-	public static $timeout = false;
 
-	/**
-	 * Set the mock values for the HTTP Mockup classes
-	 *
-	 * @param boolean $good
-	 * @param mixed $html HTML of the error message or false if no error is present.
-	 * @param boolean $timeout true if
-	 */
-	public static function setMockValues( $good, $html, $timeout ) {
-		self::$good = $good;
-		self::$html = $html;
-		self::$timeout = $timeout;
-	}
-
-	/**
-	 * Tests behavior of makeRequest() that communicates with the host.
-	 * Testcase: Invalid request.
-	 * @covers MathTexvc::makeRequest
-	 */
-	public function testMakeRequestInvalid() {
-		self::setMockValues( false, false, false );
-		$url = 'http://example.com/invalid';
-
-		$renderer = $this->getMockBuilder( 'MathLaTeXML' )
-			->setMethods( NULL )
-			->disableOriginalConstructor()
-			->getMock();
-		$requestReturn = $renderer->makeRequest( $url, 'a+b', $res, $error
-			, 'LaTeXMLHttpRequestTester' );
-		$this->assertEquals( false, $requestReturn
-			, "requestReturn is false if HTTP::post returns false." );
-		$this->assertEquals( false, $res
-			, "res is false if HTTP:post returns false." );
-		$errmsg = wfMessage( 'math_latexml_invalidresponse' , $url, '' )
-			->inContentLanguage()->escaped();
-		$this->assertContains( $errmsg, $error
-			, "return an error if HTTP::post returns false" );
-	}
-
-	/**
-	 * Tests behavior of makeRequest() that communicates with the host.
-	 * Testcase: Valid request.
-	 * @covers MathTexvc::makeRequest
-	 */
-	public function testMakeRequestSuccess() {
-		self::setMockValues( true, true, false );
-		$url = 'http://example.com/valid';
-		$renderer = $this->getMockBuilder( 'MathLaTeXML' )
-			->setMethods( NULL )
-			->disableOriginalConstructor()
-			->getMock();
-		$requestReturn = $renderer->makeRequest( $url, 'a+b', $res, $error
-			, 'LaTeXMLHttpRequestTester' );
-		$this->assertEquals( true, $requestReturn, "successful call return" );
-		$this->isTrue( $res, "successfull call" );
-		$this->assertEquals( $error, '', "successfull call errormessage" );
-	}
-
-	/**
-	 * Tests behavior of makeRequest() that communicates with the host.
-	 * Testcase: Timeout.
-	 * @covers MathLaTeXML::makeRequest
-	 */
-	public function testMakeRequestTimeout() {
-		self::setMockValues( false, true, true );
-		$url = 'http://example.com/timeout';
-		$renderer = $this->getMockBuilder( 'MathLaTeXML' )
-			->setMethods( NULL )
-			->disableOriginalConstructor()
-			->getMock();
-		$requestReturn = $renderer->makeRequest( $url, '$\longcommand$', $res
-			, $error, 'LaTeXMLHttpRequestTester' );
-		$this->assertEquals( false, $requestReturn, "timeout call return" );
-		$this->assertEquals( false, $res, "timeout call return" );
-		$errmsg = wfMessage( 'math_latexml_timeout', $url )
-			->inContentLanguage()->escaped();
-		$this->assertContains( $errmsg, $error, "timeout call errormessage" );
-	}
 
 	/**
 	 * Checks if a String is a valid MathML element
@@ -151,45 +69,5 @@ class MathLaTeXMLTest extends MediaWikiTestCase {
 		$this->assertEquals( $expected, $real
 				, "Rendering of a+b in plain Text mode." .
 				$renderer->getLastError() );
-	}
-}
-
-/**
- * Helper class for testing
- * @author physikerwelt
- * @see MWHttpRequestTester
- *
- */
-class LaTeXMLHttpRequestTester {
-	public static function factory() {
-		return new LaTeXMLHttpRequestTester();
-	}
-	public static function execute() {
-		return new LaTeXMLTestStatus();
-	}
-	public static function getContent() {
-		return MathLaTeXMLTest::$content;
-	}
-}
-
-/**
- * Helper class for testing
- * @author physikerwelt
- * @see Status
- */
-class LaTeXMLTestStatus {
-	static function isGood() {
-		return MathLaTeXMLTest::$good;
-	}
-
-	static function hasMessage( $s ) {
-		if ( $s == 'http-timed-out' ) {
-			return MathLaTeXMLTest::$timeout;
-		} else {
-			return false;
-		}
-	}
-	static function getHtml() {
-		return MathLaTeXMLTest::$html;
 	}
 }
