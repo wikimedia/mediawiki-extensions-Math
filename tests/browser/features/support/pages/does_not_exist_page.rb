94ded6c7b9c2645550cf621a09cd6776fc5d03e4
Cmcmahon <cmcmahon@wikimedia.org>
2014-06-23 10:27:07 -0700

    Port browser test for Math extension from the /qa/browsertest repo
    From an earlier discussion with physikerwelt.
    
    This is one step toward retiring the /qa/browsertest repo
    entirely.
    
    Change-Id: I804f32d2a5a984c8eb0dfc50c8ab50a7f1035256

---
+++ tests/browser/features/support/pages/does_not_exist_page.rb
@@ -0,0 +1,19 @@
+#
+# This file is subject to the license terms in the LICENSE file found in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
+# qa-browsertests, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+#
+# Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
+#
+class DoesNotExistPage
+  include PageObject
+
+  include URL
+  page_url URL.url("<%=params[:page_name]%>")
+
+  a(:create_source, text: "Create source")
+end
---
+++ tests/browser/features/support/pages/edit_page.rb
@@ -0,0 +1,18 @@
+#
+# This file is subject to the license terms in the LICENSE file found in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
+# qa-browsertests, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+#
+# Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
+#
+class EditPage
+  include PageObject
+
+  text_area(:article_text, id: "wpTextbox1")
+  img(:math_image, class: "tex")
+  button(:preview, id: "wpPreview")
+end
