1a524a5fedf3b101a7ccc03f437153a0179ba857
Gabriel Wicke <wicke@wikidev.net>
2014-10-08 16:41:35 -0700

    Bug 69702: Remove PNG image fallback image references
    - These images currently return server errors
    
    - We don't really want to load PNG images for browsers that support MathML or
      SVG.
    
    Change-Id: Idd339623efd19a0726b43a6c4adc2fcd832511f6

--- MathMathML.php
+++ MathMathML.php
@@ -448,6 +448,8 @@ class MathMathML extends MathRenderer {
 		}
 		$output .= Xml::tags( $element, array( 'class' => $this->getClassName(), 'style' => 'display: none;'  ), $mml );
 		$output .= $this->getFallbackImage( $this->getMode() ) . "\n";
+		// TODO: Implement PNG image fallbacks without loading images in
+		// browsers that support MathML or SVG!
 		$output .= $this->getFallbackImage( MW_MATH_PNG ) . "\n";
 		$output .= HTML::closeElement( $element );
 		return $output;
