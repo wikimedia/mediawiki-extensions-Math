cdb4e197b08ce8c89965cbd0268f2e7ee05ba602
Frédéric Wang <fred.wang@free.fr>
2014-10-11 17:43:34 +0200

    Experimental - SVG images should not be loaded when MathML is used
    Use span with background-image instead of <img> for the fallback SVG so
    that they don't load in Gecko.
    
    Bug: 71929
    Change-Id: I7f820cd5766db1fece452ebfc72915a55e42c82e

--- modules/ext.math.js
+++ modules/ext.math.js
@@ -1,7 +1,7 @@
 ( function ( $ ) {
 	'use strict';
 	// The MW_MATH_PNG and MW_MATH_MATHML constants are taken from Math.php
-	var MW_MATH_PNG = 0, MW_MATH_MATHML = 5, img;
+	var MW_MATH_PNG = 0, MW_MATH_MATHML = 5, img, url;
 
 	// If MathPlayer is installed we show the MathML rendering.
 	if (navigator.userAgent.indexOf('MathPlayer') > -1) {
@@ -13,10 +13,11 @@
 	// We verify whether SVG as <img> is supported and otherwise use the
 	// PNG fallback. See https://github.com/Modernizr/Modernizr/blob/master/feature-detects/svg/asimg.js
 	if (!document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1')) {
-		$( 'img.mwe-math-fallback-svg-inline, img.mwe-math-fallback-svg-display' ).each(function() {
+		$( '.mwe-math-fallback-svg-inline, .mwe-math-fallback-svg-display' ).each(function() {
 			// Create a new PNG image to use as the fallback.
 			img = document.createElement('img');
-			img.setAttribute( 'src', this.src.replace('mode=' + MW_MATH_MATHML, 'mode=' + MW_MATH_PNG) );
+			url = this.getAttribute('style').match(/url\('([^']*)'\)/)[1];
+			img.setAttribute( 'src', url.replace('mode=' + MW_MATH_MATHML, 'mode=' + MW_MATH_PNG) );
 			img.setAttribute( 'class', 'tex mwe-math-fallback-png-' + ($( this ).hasClass('mwe-math-fallback-svg-inline') ? 'inline' : 'display') );
 			img.setAttribute( 'aria-hidden', 'true' );
 			this.parentNode.insertBefore( img, this );
