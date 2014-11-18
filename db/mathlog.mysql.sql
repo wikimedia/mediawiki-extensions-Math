7a5e90f8bb62086e41554727590e965635d10e26
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2014-11-18 10:12:23 +0100

    Add logging table
    Adds a database table to store log entries in the database
    
    Change-Id: I7935f33e04a1c9d7d4f1dbbc600e840f07c19dad

---
+++ db/mathlog.mysql.sql
@@ -0,0 +1,20 @@
+--
+-- Used by the math module to oganize the log files from
+-- different rendering engines
+--
+CREATE TABLE /*_*/mathlog (
+  -- Binary MD5 hash of math_inputtex, used as an identifier key.
+  math_inputhash varbinary(16) NOT NULL,
+  -- the log input
+  math_log text NOT NULL,
+  -- the post request sent
+  math_post text,
+  -- MW_MATH_(MAHML|LATEXML)
+  math_mode tinyint
+  -- time needed to answer the request in ms
+  math_postime int,
+  -- timestamp
+  math_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  -- key
+  key ( math_inputhash, math_mode )
+) /*$wgDBTableOptions*/;
