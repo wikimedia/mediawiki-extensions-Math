d9b9b06032116967259b7f04827703d4ee3240e3
Ori Livneh <ori@wikimedia.org>
2015-01-09 17:55:54 -0800

    MWException -> Exception
    Change-Id: I18da1baeb70387b96e30811a049185a264dec26d

--- MathSource.php
+++ MathSource.php
@@ -56,7 +56,7 @@ class MathSource extends MathRenderer {
 	}
 
 	protected function getMathTableName() {
-		throw new MWException ( 'in math source mode no database caching should happen');
+		throw new Exception ( 'in math source mode no database caching should happen');
 	}
 
 	/**
