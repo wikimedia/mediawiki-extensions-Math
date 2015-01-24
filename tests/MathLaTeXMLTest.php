fabcb491bec71b1506bfec9e3057de388b5578d2
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2015-01-24 09:25:04 +0100

    Move integration tests to a new special page
    The integration tests depend on the presence of external servers
    and the user settings.
    First, people are bothered by  failing test due to problems with
    those servers and second, administrators need to run phpunit tests
    to verify the configuration settings.
    Therefore, we move the integration tests to a special page.
    
    Bug: T87389
    Change-Id: If66f13fe7fa16868cd4b1d8f0e0a6e3d49448b27

--- tests/MathLaTeXMLTest.php
+++ tests/MathLaTeXMLTest.php
@@ -33,20 +33,4 @@ class MathLaTeXMLTest extends MediaWikiTestCase {
 			'test serialization of a string setting'
 		);
 	}
-
-	/**
-	 * Checks the basic functionality
-	 * i.e. if the span element is generated right.
-	 */
-	public function testIntegration() {
-		$this->setMwGlobals( 'wgMathLaTeXMLTimeout', 20 );
-		$this->setMwGlobals( 'wgMathValidModes', array( MW_MATH_LATEXML ) );
-		$renderer = MathRenderer::getRenderer( "a+b", array(), MW_MATH_LATEXML );
-		$this->assertTrue( $renderer->render( true ) );
-		$expected = '<math xmlns="http://www.w3.org/1998/Math/MathML" id="p1.1.m1.1" class="ltx_Math" alttext="{\displaystyle a+b}" ><semantics id="p1.1.m1.1a"><mrow id="p1.1.m1.1.4" xref="p1.1.m1.1.4.cmml"><mi id="p1.1.m1.1.1" xref="p1.1.m1.1.1.cmml">a</mi><mo id="p1.1.m1.1.2" xref="p1.1.m1.1.2.cmml">+</mo><mi id="p1.1.m1.1.3" xref="p1.1.m1.1.3.cmml">b</mi></mrow><annotation-xml encoding="MathML-Content" id="p1.1.m1.1b"><apply id="p1.1.m1.1.4.cmml" xref="p1.1.m1.1.4"><plus id="p1.1.m1.1.2.cmml" xref="p1.1.m1.1.2"/><ci id="p1.1.m1.1.1.cmml" xref="p1.1.m1.1.1">a</ci><ci id="p1.1.m1.1.3.cmml" xref="p1.1.m1.1.3">b</ci></apply></annotation-xml><annotation encoding="application/x-tex" id="p1.1.m1.1c">{\displaystyle a+b}</annotation></semantics></math>';
-		$real = preg_replace( "/\n\s*/", '', $renderer->getHtmlOutput() );
-		$this->assertContains( $expected, $real
-			, "Rendering of a+b in plain Text mode." .
-			$renderer->getLastError() );
-	}
 }
