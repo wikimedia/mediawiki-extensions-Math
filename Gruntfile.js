fe3165faff156f9699847aa831cfbe9515f75d29
Thalia <thalia.e.chan@googlemail.com>
2015-09-14 19:46:13 +0100

    build: Add csshint, jshint and jscs to math extension
    Also adds the corrections made to various js files when jscs
    was run, and removes an old version of .jshintrc. Only
    VisualEditor files are checked by csshint due to clashes with
    an upstream bug in the csslint parser. This could be looked
    into in the future.
    
    Bug: T112576
    Change-Id: I611901040d3b38ae3ef707deb8d25771f043b1d8

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-contrib-csslint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	grunt.initConfig( {
 		banana: {
@@ -12,9 +16,43 @@ module.exports = function ( grunt ) {
 				'**/*.json',
 				'!node_modules/**'
 			]
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			all: 'modules/VisualEditor/*.css'
+		},
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'modules/**/*.js'
+			]
+		},
+		watch: {
+			files: [
+				'.{csslintrc,jscsrc,jshintignore,jshintrc}',
+				'<%= jshint.all %>',
+				'<%= csslint.all %>'
+			],
+			tasks: 'test'
+		},
+		jscs: {
+			fix: {
+				options: {
+					fix: true
+				},
+				src: '<%= jshint.all %>'
+			},
+			main: {
+				src: '<%= jshint.all %>'
+			}
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs:main', 'csslint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
