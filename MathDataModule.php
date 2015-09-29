12f0bd633575aaa77d270f3262b8762f2f23f10d
Thalia <thalia.e.chan@googlemail.com>
2015-09-29 16:44:21 +0100

    WIP Make math dialog with clickable list of formula fragments
    The main components are:
    - a menu of formula fragment buttons
    - an ace editor text input into which LaTeX commands are
    inserted on clicking the buttons
    - a preview of the formula
    
    The deviations from the draft design (full-width menu,
    smaller buttons, dialog is 'larger' instead of 'large')
    are so that more buttons can be seen at once.
    
    Needs more work:
    - the menu headings
    - the list of formula fragments, which will eventually
    be more comprehensive
    - user research on how to group/order the fragments
    
    Depends on Ia0f46da2d4 in ve-mw
    Depends on Ib75fb7b087 in ve-mw
    Depends on I758f90abb9 in ve-mw
    Depends on Ibcd941d938 in CodeEditor
    
    Bug: T114163
    Change-Id: I96d4b72f2e49ad3f43a2e5c71f0d348fdfb35503

---
+++ MathDataModule.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Resource loader module providing extra data from the server to Math.
+ *
+ * @file
+ * @ingroup Extensions
+ * @copyright 2011-2015 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+class MathDataModule extends ResourceLoaderModule {
+
+	/* Protected Members */
+
+	protected $origin = self::ORIGIN_USER_SITEWIDE;
+	protected $targets = array( 'desktop', 'mobile' );
+
+	/* Methods */
+
+	public function getScript( ResourceLoaderContext $context ) {
+		return
+			've.ui.MWMathDialog.static.setSymbols(' . file_get_contents( __DIR__ . '/modules/ve-math/symbols.json' ) . ');';
+	}
+
+	public function getDependencies( ResourceLoaderContext $context = null ) {
+		return array(
+			'ext.math.visualEditor',
+		);
+	}
+
+	public function getDefinitionSummary( ResourceLoaderContext $context ) {
+		$summary = parent::getDefinitionSummary( $context );
+		$summary[] = array(
+			'script' => $this->getScript( $context ),
+		);
+		return $summary;
+	}
+}
