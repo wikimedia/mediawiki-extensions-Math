bc068a75c9d1b5bc63fe7122bb43bc5e2d67acad
lyrianos93 <dlinhtran@hotmail.de>
2015-12-14 20:26:29 +0000

    Implement datatype 'Math' for Wikidata
    This change implements all components to use the datatype 'Math' in Wikidata.
    Because 'String 'is used as value type, only the Formatter and Validator are needed to be implemended.
    
    The components are:
    * hooks
    * Formatter class
    * Validator class
    
    Bug: T67397
    Change-Id: Ic64fd6c8560f48052e2db24ae1f013d48a82b5e9

---
+++ MathWikidataHook.php
@@ -0,0 +1,60 @@
+<?php
+
+use ValueFormatters\FormatterOptions;
+use ValueParsers\StringParser;
+use Wikibase\Repo\Parsers\WikibaseStringValueNormalizer;
+use Wikibase\Repo\Validators\CompositeValidator;
+use Wikibase\Repo\WikibaseRepo;
+
+class MathWikidataHook {
+	/*
+	 * Add Datatype "Math" to the Wikibase Repository
+	 */
+	public static function onWikibaseRepoDataTypes( array &$dataTypeDefinitions ) {
+		$dataTypeDefinitions['PT:math'] = array(
+			'value-type'                 => 'string',
+			'validator-factory-callback' => function() {
+				// load validator builders
+				$factory = WikibaseRepo::getDefaultValidatorBuilders();
+
+				// initialize an array with string validators
+				// returns an array of validators
+				// that add basic string validation such as preventing empty strings
+				$validators = $factory->buildStringValidators();
+				$validators[] = new MathValidator();
+
+				// return a composite validator which calls the validate()-functions
+				// of registered validators iterative
+				// and fails if one sub-validator does
+				return array( new CompositeValidator( $validators ) );
+			},
+			'parser-factory-callback' => function( ParserOptions $options ) {
+				$repo = WikibaseRepo::getDefaultInstance();
+				$normalizer = new WikibaseStringValueNormalizer( $repo->getStringNormalizer() );
+				return new StringParser( $normalizer );
+			},
+			'formatter-factory-callback' => function( $format, FormatterOptions $options ) {
+				global $wgOut;
+				$styles = array( 'ext.math.desktop.styles', 'ext.math.scripts', 'ext.math.styles' );
+				$wgOut->addModuleStyles( $styles );
+				return new MathFormatter( $format );
+			},
+		);
+	}
+
+	/*
+     * Add Datatype "Math" to the Wikibase Client
+     */
+	public static function onWikibaseClientDataTypes( array &$dataTypeDefinitions ) {
+		$dataTypeDefinitions['PT:math'] = array(
+			'value-type'                 => 'string',
+			'formatter-factory-callback' => function( $format, FormatterOptions $options ) {
+				global $wgOut;
+				$styles = array( 'ext.math.desktop.styles', 'ext.math.scripts', 'ext.math.styles' );
+				$wgOut->addModuleStyles( $styles );
+				return new MathFormatter( $format );
+			},
+		);
+	}
+
+}
