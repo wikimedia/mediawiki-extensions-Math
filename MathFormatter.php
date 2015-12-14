323010c95a7bc332f9ef36d615799f99cb8a892c
lyrianos93 <dlinhtran@hotmail.de>
2015-12-14 20:26:29 +0000

    Implement datatype 'Math' for Wikidata
    This change implements all components to use the datatype 'Math' in Wikidata.
    Because 'String 'is used as value type, only the Formatter and Validator are needed to be implemended.
    
    The components are:
    * hooks
    * Formatter class
    * Validator class
    
    Bug: T67397
    Change-Id: Ic64fd6c8560f48052e2db24ae1f013d48a82b5e9

---
+++ MathFormatter.php
@@ -0,0 +1,72 @@
+<?php
+
+use DataValues\StringValue;
+use ValueFormatters\FormattingException;
+use ValueFormatters\ValueFormatter;
+use DataValues\IllegalValueException;
+
+/*
+* Formats the tex string based on the known formats
+* * text/plain: used in the value input field of Wikidata
+* * text/x-wiki: wikitext
+* * text/html: used in Wikidata to display the value of properties
+* Formats can look like this: "text/html; disposition=widget"
+* or just "text/plain"
+*/
+
+class MathFormatter implements ValueFormatter {
+
+	private $baseFormat;
+
+	/*
+	 * Loads format to distinguish the type of formatting
+	 */
+	public function __construct( $format ) {
+		// purge format
+		$this->baseFormat = preg_replace( '/ *;.*/', '', $format );
+		if ( !in_array( $this->baseFormat, array(
+				'text/x-wiki', 'text/plain', 'text/html' ), true ) ){
+			// unknown format: throw FormattingException
+			throw new FormattingException();
+		}
+	}
+
+	/*
+	 *
+	 * @param StringValue $value
+	 *
+	 * @return string
+	 * @throws \ValueFormatters\FormattingException
+	 */
+	public function format( $value ) {
+
+		if ( !( $value instanceof StringValue ) ) {
+			throw new IllegalValueException( '$value must be a StringValue' );
+		}
+		$tex = $value->getValue();
+
+		switch ( $this->baseFormat ) {
+			case "text/plain":
+				return "$tex";
+			case "text/x-wiki":
+				return "<math>$tex</math>";
+			case "text/html":
+				$renderer = new MathMathML( $tex );
+				if ( $renderer->checkTex() ) {
+					if ( $renderer->render() ) {
+						return $renderer->getHtmlOutput();
+					}
+				}
+				// TeX string is not valid or rendering failed
+				return $renderer->getLastError();
+		}
+	}
+
+	/**
+	 *
+	 * @return format
+	 */
+	public function getFormat() {
+		return $this->format;
+	}
+}
