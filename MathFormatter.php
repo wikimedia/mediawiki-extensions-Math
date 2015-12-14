ef64860db84ab95419df9cb005634bfe4de0049f
lyrianos93 <dlinhtran@hotmail.de>
2015-12-14 20:26:29 +0000

    WIP: Implement Datatype 'Math' for Wikidata
    All components to use the Math Datatype in Wikidata.
    Because 'String 'is used as value type, only the Formatter and Validator are needed to be implemended.
    
    Things we have done so far:
    * Add hooks to extension
    * Add Formatter class
    * Add Validator class
    * Add Test for Formatter
    
    Things to do:
    * Add Test for Validator
    
    Bug: T67397
    Change-Id: Ic64fd6c8560f48052e2db24ae1f013d48a82b5e9

---
+++ MathFormatter.php
@@ -0,0 +1,76 @@
+<?php
+
+use ValueFormatters\ValueFormatter;
+use DataValues\IllegalValueException;
+
+// @author Duc Linh Tran, Julian Hilbig, Moritz Schubotz
+
+class MathFormatter implements ValueFormatter {
+
+	private $format;
+
+	/*
+	 * Loads format to distinguish the type of formatting
+	 */
+	public function __construct( $format ) {
+		$this->format = $format;
+	}
+
+	/*
+	 * Formats the tex string based on the known formats
+	 *** text/plain: used in the value input field of Wikidata
+	 *** text/x-wiki: used in articles to display text to readers
+	 *** text/html: used in Wikidata to display the value of properties
+	 * Formats can look like this: "text/html; disposition=widget"
+	 * or just "text/plain"
+	 *
+	 * @param string $value
+	 *
+	 * @return string
+	 * @throws \ValueFormatters\FormattingException
+	 */
+	public function format( $value ) {
+		if ( !( $value instanceof StringValue ) ) {
+			throw new IllegalValueException( '$value must be a StringValue' );
+		}
+		$tex = $value->getValue();
+
+		$clearFormat = array();
+		// extract clear format from format-string
+		// only formats with prefix "text/" are supported yet
+		// keep dash and semicolon in mind
+		preg_match( '/text\/([A-Za-z\-]+)(;|$|\s)/', $this->format, $clearFormat );
+
+		switch ( $clearFormat[1] ) {
+			case "plain":
+				return "$tex";
+				break;
+			case "x-wiki":
+				return "<math>$tex</math>";
+				break;
+			case "html":
+				$renderer =  new MathMathML( $tex );
+				if ( $renderer->checkTex() ) {
+					if ( $renderer->render() ){
+						return $renderer->getHtmlOutput();
+					}
+				}
+				return $renderer->getLastError();
+				break;
+			default:
+				// unknown format: return error message
+				return "<strong class='error texerror'>" .
+					"Unknown format " . $this->format .
+					"</strong>";
+		}
+	}
+
+	/**
+	 * Gets format
+	 *
+	 * @return format
+	 */
+	public function getFormat() {
+		return $this->format;
+	}
+}
