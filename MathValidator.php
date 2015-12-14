8f2a16b5a1d19381cdaa98f9d1fae0200e0f1b7b
lyrianos93 <dlinhtran@hotmail.de>
2015-12-14 20:26:29 +0000

    Implement datatype 'Math' for Wikidata
    This change implements all components to use the datatype 'Math' in Wikidata.
    Because 'String 'is used as value type, only the Formatter and Validator are needed to be implemended.
    
    The components are:
    * hooks
    * Formatter class
    * Validator class
    * Test cases
    
    Bug: T67397
    Change-Id: Ic64fd6c8560f48052e2db24ae1f013d48a82b5e9

---
+++ MathValidator.php
@@ -0,0 +1,49 @@
+<?php
+
+use DataValues\IllegalValueException;
+use ValueValidators\Result;
+use ValueValidators\ValueValidator;
+use ValueValidators\Error;
+use DataValues\StringValue;
+
+// @author Duc Linh Tran, Julian Hilbig, Moritz Schubotz
+
+class MathValidator implements ValueValidator {
+
+	/**
+	 * Validates a value with MathInputCheckRestbase
+	 *
+	 * @param mixed $value The value to validate
+	 *
+	 * @return \ValueValidators\Result
+	 */
+	public function validate( $value ) {
+		if ( !( $value instanceof StringValue ) ) {
+			throw new IllegalValueException( '$value must be a StringValue' );
+		}
+
+		// get input String from value
+		$tex = $value->getValue();
+
+		$checker = new MathInputCheckRestbase( $tex );
+		if ( $checker->isValid() )  {
+			return Result::newSuccess();
+		}
+
+		// TeX string is not valid
+		return Result::newError(
+			array(
+				Error::newError( null, null, 'malformed-value', $checker->getError() )
+			)
+		);
+	}
+
+	/**
+	 * @see ValueValidator::setOptions()
+	 *
+	 * @param array $options
+	 */
+	public function setOptions( array $options ) {
+		// Do nothing. This method shouldn't even be in the interface.
+	}
+}
