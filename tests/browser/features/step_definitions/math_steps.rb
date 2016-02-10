77804a0ed55478799497ddd25fcfc5afc7b3110d
Željko Filipin <zeljko.filipin@gmail.com>
2016-02-10 17:22:37 +0100

    Fixed broken Selenium test
    The problems was that visual editor is the default editor at
    en.wikipedia.beta.wmflabs.org, and the test was expecting source editor.
    
    The test now works also locally, using mediawiki-vagrant.
    
    Removed headers from some files. We no longer use those headers.
    
    Removed cucumber tags we no longer use, mostly internet explorer.
    
    Added missing dependency (rake) to gemfile.
    
    Bug: T126463
    Change-Id: Ia5809f40719a2fe0c6c94338072a38765fa5c87b

--- tests/browser/features/step_definitions/math_steps.rb
+++ tests/browser/features/step_definitions/math_steps.rb
@@ -1,16 +1,4 @@
-#
-# This file is subject to the license terms in the LICENSE file found in the
-# qa-browsertests top-level directory and at
-# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
-# qa-browsertests, including this file, may be copied, modified, propagated, or
-# distributed except according to the terms contained in the LICENSE file.
-#
-# Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
-# qa-browsertests top-level directory and at
-# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
-#
-
-Given(/^I am at page that does not exist$/) do
+Given(/^I am creating a page with source editor$/) do
   visit(DoesNotExistPage, using_params: { page_name: @random_string })
 end
 
@@ -30,10 +18,9 @@ Then(/^alt for that img should be (.+)$/) do |alt|
   expect(on(EditPage).math_image_element.element.alt).to eq(alt)
 end
 
-Then(/^src for that img should come from (.+)$/) do |src|
+Then(/^src for that img should contain (.+)$/) do |src|
   expect(on(EditPage).math_image_element.element.src).to match Regexp.escape src
 end
-
 
 Then(/^the page should contain an img tag$/) do
   expect(on(EditPage).math_image_element.when_present).to be_visible
