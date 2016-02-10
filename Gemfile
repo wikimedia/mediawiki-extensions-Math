11825b9c178efb4062f02050bb121997d5104807
Željko Filipin <zeljko.filipin@gmail.com>
2016-02-10 17:22:37 +0100

    Fixed broken Selenium test
    The problems was that visual editor is the default editor at
    en.wikipedia.beta.wmflabs.org, and the test was expecting source editor.
    
    The test now works also locally, using mediawiki-vagrant.
    
    Removed headers from some files. We no longer use those headers.
    
    Removed cucumber tags we no longer use, mostly internet explorer.
    
    Added missing dependency (rake) to gemfile.
    
    Bug: T126463
    Change-Id: Ia5809f40719a2fe0c6c94338072a38765fa5c87b

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'mediawiki_selenium', '~> 1.6.5'
+gem 'rake', '~> 10.5'
 gem 'rubocop', '~> 0.33.0', require: false
--- tests/browser/features/support/pages/does_not_exist_page.rb
+++ tests/browser/features/support/pages/does_not_exist_page.rb
@@ -1,18 +1,5 @@
-#
-# This file is subject to the license terms in the LICENSE file found in the
-# qa-browsertests top-level directory and at
-# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
-# qa-browsertests, including this file, may be copied, modified, propagated, or
-# distributed except according to the terms contained in the LICENSE file.
-#
-# Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
-# qa-browsertests top-level directory and at
-# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
-#
 class DoesNotExistPage
   include PageObject
 
-  page_url '<%=params[:page_name]%>'
-
-  a(:action_edit, css: 'li#ca-edit > span > a')
+  page_url '<%=params[:page_name]%>?action=edit'
 end
