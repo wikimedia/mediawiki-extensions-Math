2fc8fa6ccdba74fadcc4d38db5d453ea5d12b3d6
Moritz Schubotz <wiki@physikerwelt.de>
2016-06-03 10:10:06 +0200

    Fix: Update png fallback
    In Ifdd75ce51076b098af67a96b5dcb0fc0726d2b00
    I missed to update the png fallback mechanism
    given the fact that SVG is now a <img element
    as well and no longer a <meta tag.
    
    Bug: T136905
    Change-Id: If1b4fb11cfb50eab98f847674fdbef2cd0e43e37

--- modules/ext.math.js
+++ modules/ext.math.js
@@ -1,24 +1,5 @@
 ( function ( $ ) {
 	'use strict';
-	var img, url;
-
-	function insertImg( png ) {
-		$( '.mwe-math-fallback-image-inline, .mwe-math-fallback-image-display' ).each( function ( ) {
-			// Create a new image to use as the fallback.
-			img = document.createElement( 'img' );
-			url = this.style.backgroundImage.match( /url\(\s*(['"]?)([^\1\)]*)\1\s*\)/ )[ 2 ];
-			if ( png ) {
-				url = url.replace( 'media/math/render/svg/', 'media/math/render/png/' );
-			}
-			img.setAttribute( 'src', url );
-			img.setAttribute( 'class', 'tex mwe-math-fallback-image-' + ( $( this ).hasClass( 'mwe-math-fallback-image-inline' ) ? 'inline' : 'display' ) );
-			img.setAttribute( 'aria-hidden', 'true' );
-			this.parentNode.insertBefore( img, this );
-
-			// Hide the old SVG fallback.
-			$( this ).css( 'display', 'none' );
-		} );
-	}
 
 	// If MathPlayer is installed we show the MathML rendering.
 	if ( navigator.userAgent.indexOf( 'MathPlayer' ) > -1 ) {
@@ -30,6 +11,8 @@
 	// We verify whether SVG as <img> is supported and otherwise use the
 	// PNG fallback. See https://github.com/Modernizr/Modernizr/blob/master/feature-detects/svg/asimg.js
 	if ( !document.implementation.hasFeature( 'http://www.w3.org/TR/SVG11/feature#Image', '1.1' ) ) {
-		insertImg( true );
+		$( '.mwe-math-fallback-image-inline, .mwe-math-fallback-image-display' ).each( function ( ) {
+			this.src = this.src.replace( 'media/math/render/svg/', 'media/math/render/png/' );
+		} );
 	}
 }( jQuery ) );
