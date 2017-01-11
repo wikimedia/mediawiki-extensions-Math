837e4b6cbc4874c42af3925adc2feb973b6cfa18
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-01-11 13:50:35 -0800

    [WIP] Add support for chemical formula in VisualEditor
    The chemical and mathematical formula inspectors and dialogs
    have a lot of similar functionality, and the common parts are
    now moved to the common MWLatex class.
    
    MWMath* classes now inherit from MWLatex*, and so do
    the new MWChem* classes.
    
    TODO:
    * Make MWMathDialog.static.setSymbols generic.
    * Create symbols for chem.
    * Split MathPage and ChemPage.
    * Split DialogTool
    
    Bug: T153365
    Change-Id: I4452ceca55197fda5f1e1293a5741c6f5fb7c245

--- modules/ve-math/ve.ui.MWMathPage.js
+++ modules/ve-math/ve.ui.MWMathPage.js
@@ -16,63 +16,10 @@
  * @param {Object} [config] Configuration options
  */
 ve.ui.MWMathPage = function VeUiMWMathPage( name, config ) {
-	var i, ilen, j, jlen, symbol, symbols, $symbols,
-		symbolNode, symbolsNode, tex, classes;
-
 	// Parent constructor
 	ve.ui.MWMathPage.super.call( this, name, config );
-
-	this.label = config.label;
-
-	symbols = config.symbols;
-	$symbols = $( '<div>' ).addClass( 've-ui-specialCharacterPage-characters' );
-	symbolsNode = $symbols[ 0 ];
-
-	// Avoiding jQuery wrappers as advised in ve.ui.SpecialCharacterPage
-	for ( i = 0, ilen = symbols.length; i < ilen; i++ ) {
-		symbol = symbols[ i ];
-		if ( !symbol.notWorking && !symbol.duplicate ) {
-			tex = symbol.tex;
-			classes = [ 've-ui-mwMathPage-symbol' ];
-			classes.push(
-				've-ui-mwMathSymbol-' + tex.replace( /[^\w]/g, function ( c ) {
-					return '_' + c.charCodeAt( 0 ) + '_';
-				} )
-			);
-			if ( symbol.width ) {
-				classes.push( 've-ui-mwMathPage-symbol-' + symbol.width );
-			}
-			if ( symbol.contain ) {
-				classes.push( 've-ui-mwMathPage-symbol-contain' );
-			}
-			if ( symbol.largeLayout ) {
-				classes.push( 've-ui-mwMathPage-symbol-largeLayout' );
-			}
-			symbolNode = document.createElement( 'div' );
-			for ( j = 0, jlen = classes.length; j < jlen; j++ ) {
-				symbolNode.classList.add( classes[ j ] );
-			}
-			$.data( symbolNode, 'symbol', symbol );
-			symbolsNode.appendChild( symbolNode );
-		}
-	}
-
-	this.$element
-		.addClass( 've-ui-mwMathPage' )
-		.append( $( '<h3>' ).text( name ), $symbols );
 };
 
 /* Inheritance */
 
-OO.inheritClass( ve.ui.MWMathPage, OO.ui.PageLayout );
-
-/* Methods */
-
-/**
- * @inheritdoc
- */
-ve.ui.MWMathPage.prototype.setupOutlineItem = function ( outlineItem ) {
-	ve.ui.MWMathPage.super.prototype.setupOutlineItem.call( this, outlineItem );
-	this.outlineItem.setLabel( this.label );
-	this.outlineItem.$element.addClass( 've-ui-mwMathPage-outline' );
-};
+OO.inheritClass( ve.ui.MWMathPage, ve.ui.MWLatexPage );
