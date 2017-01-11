f2120996fec28c1227e7b7177e74e0d51fef4340
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-01-11 13:50:35 -0800

    [WIP] Add support for chemical formula in VisualEditor
    The chemical and mathematical formula inspectors and dialogs
    have a lot of similar functionality, and the common parts are
    now moved to the common MWLatex class.
    
    MWMath* classes now inherit from MWLatex*, and so do
    the new MWChem* classes.
    
    TODO:
    * Make MWMathDialog.static.setSymbols generic.
    * Create symbols for chem.
    * Split MathPage and ChemPage.
    * Split DialogTool
    
    Bug: T153365
    Change-Id: I4452ceca55197fda5f1e1293a5741c6f5fb7c245

---
+++ modules/ve-math/ve.ce.MWChemNode.js
@@ -0,0 +1,35 @@
+/*!
+ * VisualEditor ContentEditable MWChemNode class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * ContentEditable MediaWiki chem node.
+ *
+ * @class
+ * @extends ve.ce.MWInlineExtensionNode
+ *
+ * @constructor
+ * @param {ve.dm.MWChemNode} model Model to observe
+ * @param {Object} [config] Configuration options
+ */
+ve.ce.MWChemNode = function VeCeMWChemNode() {
+	// Parent constructor
+	ve.ce.MWChemNode.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.MWChemNode, ve.ce.MWLatexNode );
+
+/* Static Properties */
+
+ve.ce.MWChemNode.static.name = 'mwChem';
+
+ve.ce.MWChemNode.static.primaryCommandName = 'chemDialog';
+
+/* Registration */
+
+ve.ce.nodeFactory.register( ve.ce.MWChemNode );
