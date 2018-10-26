4a25fa88a16da0987f735ca72d06dfb21c2453d7
Paladox <thomasmulhall410@yahoo.com>
2018-10-26 15:44:05 +0000

    Use "CREATE TABLE IF NOT EXISTS" instead of "CREATE TABLE"
    This is to prevent it erroring out if the table exists.
    
    MW supports mysql 5.5+ so this should be ok.
    
    Change-Id: I9076ff62a50b7f4dd0d0900d32ddb6aa74410bd1

--- db/mathoid.mysql.sql
+++ db/mathoid.mysql.sql
@@ -2,7 +2,7 @@
 -- Used by the math module to keep track
 -- of previously-rendered items.
 --
-CREATE TABLE /*_*/mathoid (
+CREATE TABLE IF NOT EXISTS /*_*/mathoid (
   -- Binary MD5 hash of math_inputtex, used as an identifier key.
   math_inputhash varbinary(16) NOT NULL PRIMARY KEY,
   -- the user input
