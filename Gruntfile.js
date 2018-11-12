c0beeff00568e116d201ca9136ea96553b9bda85
Ed Sanders <esanders@wikimedia.org>
2018-11-12 16:21:34 +0000

    eslint: Use in all folders (including tests/selenium)
    Change-Id: I0ab0a9f99817077934e7569d19d76735c33678e9

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-/* eslint-env node */
+/* eslint-env node, es6 */
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 
@@ -34,8 +34,7 @@ module.exports = function ( grunt ) {
 		},
 		eslint: {
 			all: [
-				'*.js',
-				'modules/**/*.js',
+				'**/*.js',
 				'!**/node_modules/**',
 				'!vendor/**'
 			]
