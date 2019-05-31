b9902ec775cee05618068fd9c4dcad556c40cc97
Umherirrender <umherirrender_de.wp@web.de>
2019-05-31 20:35:50 +0200

    Add phan
    Change-Id: I3557e798690f895bd7e8b3caf49d788c66a6a374

---
+++ .phan/config.php
@@ -0,0 +1,25 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['file_list'][] = 'MathLaTeXML.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/VisualEditor',
+		'../../extensions/Wikibase/repo',
+		'../../extensions/Wikibase/lib',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/VisualEditor',
+		'../../extensions/Wikibase/repo',
+		'../../extensions/Wikibase/lib',
+	]
+);
+
+return $cfg;
