796a02173d409574c995bfa91a1523d258dd7445
Marko Obrovac <mobrovac@wikimedia.org>
2019-11-14 17:16:03 +0100

    Minor: Only log the code and body of the response in case of error
    Bug: T238344
    Change-Id: Ibf769f9f4f52062a7914f257979f510c809cbe37

--- src/MathRestbaseInterface.php
+++ src/MathRestbaseInterface.php
@@ -448,8 +448,8 @@ class MathRestbaseInterface {
 		// Remove "convenience" duplicate keys put in place by MultiHttpClient
 		unset( $response[0], $response[1], $response[2], $response[3], $response[4] );
 		$this->log()->error( 'Restbase math server problem:', [
-			'request' => $request,
-			'response' => $response,
+			'url' => $request['url'],
+			'response' => [ 'code' => $response['code'], 'body' => $response['body'] ],
 			'math_type' => $type,
 			'tex' => $this->tex
 		] );
