0902318fcdc424b119982d037a10c8245cd5fb17
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-06-17 22:48:43 +0200

    WIP: Add mathoid render REST endpoint
    needs:
    * test
    * documentation
    
    Change-Id: I47d201d65e2064d21cc5d311ffe3018c5fc77ce8

---
+++ src/Rest/HashLookupHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MediaWiki\Extension\Math\Rest;
+
+use MediaWiki\Rest\LocalizedHttpException;
+use MediaWiki\Rest\Response;
+use MediaWiki\Rest\SimpleHandler;
+use WANObjectCache;
+use Wikimedia\Message\MessageValue;
+use Wikimedia\ParamValidator\ParamValidator;
+
+class HashLookupHandler extends SimpleHandler {
+	/** @var WANObjectCache */
+	private $cache;
+
+	public function __construct( WANObjectCache $cache ) {
+		$this->cache = $cache;
+	}
+
+	public function getParamSettings() {
+		return [
+			'hash' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_REQUIRED => true,
+			]
+		];
+	}
+
+	public function run( string $hash ) : array {
+		$content = $this->cache->get( $this->cache->makeGlobalKey( CheckHandler::class, $hash ) );
+		if ( $content === false ) {
+			throw new LocalizedHttpException(
+				new MessageValue( 'math-rest-nonexistent-hash', [ $hash ] ), 404 );
+		}
+		return $content;
+	}
+
+}
