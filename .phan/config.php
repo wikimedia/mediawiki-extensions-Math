efe35e9a09fe3e8423fecea4e6626bfe627a0c6d
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-08-19 20:00:29 +0200

    build: Remove phan warning
    MathLaTeXML is now in src. Thus the extra phan config is
    no longer required.
    
    Change-Id: Iceddc2582893fb40d02abc3f406db42c740499b8

--- .phan/config.php
+++ .phan/config.php
@@ -2,8 +2,6 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['file_list'][] = 'MathLaTeXML.php';
-
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
