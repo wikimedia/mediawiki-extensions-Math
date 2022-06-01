94920dfb07160f20d3e1d20c017b7927dbfea574
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-06-01 10:39:29 +0200

    Set up mathoid and mathlatexml regardless of configuration
    Set up both database tables (mathoid and mathlatexm) regardless of the enabled modes. This will allow to change configuration without having to rerun db update scripts for the price of having potentially empty tables in the db.
    
    Bug: T307896
    Change-Id: I5ed50e53835c378f9f497a6ff3a4195299259d5b
    (cherry picked from commit 92c4db78ef53b78326d398e2976ea265b25330fd)

--- src/Hooks.php
+++ src/Hooks.php
@@ -21,7 +21,6 @@ class Hooks {
 	 * MaintenanceRefreshLinksInit handler; optimize settings for refreshLinks batch job.
 	 *
 	 * @param Maintenance $maint
-	 * @return bool hook return code
 	 */
 	public static function onMaintenanceRefreshLinksInit( $maint ) {
 		$user = RequestContext::getMain()->getUser();
@@ -29,7 +28,6 @@ class Hooks {
 		// Don't parse LaTeX to improve performance
 		MediaWikiServices::getInstance()->getUserOptionsManager()
 			->setOption( $user, 'math', MathConfig::MODE_SOURCE );
-		return true;
 	}
 
 	/**
@@ -37,56 +35,43 @@ class Hooks {
 	 *
 	 * @param DatabaseUpdater $updater
 	 * @throws Exception
-	 * @return bool
 	 */
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
-		// This hook runs before services are initialized, so we cannot
-		// use MathConfig service directly.
-		global $wgMathValidModes;
-
 		$type = $updater->getDB()->getType();
-		$validModes = array_map( static function ( $mode ) {
-			return MathConfig::normalizeRenderingMode( $mode );
-		}, $wgMathValidModes );
-		if ( in_array( MathConfig::MODE_LATEXML, $validModes ) ) {
-			if ( in_array( $type, [ 'mysql', 'sqlite', 'postgres' ] ) ) {
-				$sql = __DIR__ . '/../db/mathlatexml.' . $type . '.sql';
-				$updater->addExtensionTable( 'mathlatexml', $sql );
-				if ( $type == 'mysql' ) {
-					$sql = __DIR__ . '/../db/patches/mathlatexml.mathml-length-adjustment.mysql.sql';
-					$updater->modifyExtensionField( 'mathlatexml', 'math_mathml', $sql );
-				}
-			} else {
-				throw new Exception( "Math extension does not currently support $type database for LaTeXML." );
-			}
-		}
-		if ( in_array( MathConfig::MODE_MATHML, $validModes ) ) {
-			if ( in_array( $type, [ 'mysql', 'sqlite', 'postgres' ] ) ) {
-				$sql = __DIR__ . '/../db/mathoid.' . $type . '.sql';
-				$updater->addExtensionTable( 'mathoid', $sql );
-				if ( $type == 'mysql' ) {
-					$sql = __DIR__ . '/../db/patches/mathoid.add_png.mysql.sql';
-					$updater->addExtensionField( 'mathoid', 'math_png', $sql );
-				}
-			} else {
-				throw new Exception( "Math extension does not currently support $type database for Mathoid." );
-			}
+		if ( !in_array( $type, [ 'mysql', 'sqlite', 'postgres' ] ) ) {
+			throw new Exception( "Math extension does not currently support $type database." );
 		}
 
-		return true;
+		foreach ( [ 'mathoid', 'mathlatexml' ] as $mode ) {
+			$updater->addExtensionTable(
+				$mode,
+				__DIR__ . "/../db/$mode.$type.sql"
+			);
+		}
+
+		if ( $type === 'mysql' ) {
+			$updater->modifyExtensionField(
+				'mathlatexml',
+				'math_mathml',
+				__DIR__ . '/../db/patches/mathlatexml.mathml-length-adjustment.mysql.sql'
+			);
+			$updater->addExtensionField(
+				'mathoid',
+				'math_png',
+				__DIR__ . '/../db/patches/mathoid.add_png.mysql.sql'
+			);
+		}
 	}
 
 	/**
 	 * Remove Special:MathWikibase if the Wikibase client extension isn't loaded
 	 *
 	 * @param array &$list
-	 * @return bool true
 	 */
 	public static function onSpecialPageInitList( &$list ) {
 		if ( !ExtensionRegistry::getInstance()->isLoaded( 'WikibaseClient' ) ) {
 			unset( $list['MathWikibase'] );
 		}
-		return true;
 	}
 
 }
