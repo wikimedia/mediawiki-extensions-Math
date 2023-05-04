38e649c7220a95eeb08c130f8302a3629769db4b
daniel <dkinzler@wikimedia.org>
2023-05-04 23:24:34 +0200

    Math: test usage of URL in HttpMultiClient
    Change-Id: I063319ad1b12d66d2054846e6c3b27c715ca3d31
    Co-Authored-By: <rrana-ctr@wikimedia.org>

--- tests/phpunit/MathRestbaseInterfaceTest.php
+++ tests/phpunit/MathRestbaseInterfaceTest.php
@@ -101,4 +101,177 @@ class MathRestbaseInterfaceTest extends MediaWikiIntegrationTestCase {
 		$this->expectExceptionMessage( 'Cannot get mml. Server problem.' );
 		MathRestbaseInterface::throwContentError( 'mml', $input );
 	}
+
+	public function dataProviderForTestUrlUsedByCheckTeX() {
+		$path = 'media/math/check/tex';
+
+		yield 'Math FullRestbaseURL default case' => [
+			[],
+			[
+				'url' => "https://wikimedia.org/api/rest_v1/$path",
+				'method' => 'POST',
+				'body' => [ 'type' => 'tex', 'q' => '\sin\newcommand' ]
+			],
+		];
+
+		yield 'Math FullRestbaseURL case' => [
+			[
+				'MathFullRestbaseURL' => 'https://myWiki.test/'
+			],
+			[
+				'url' => "https://myWiki.test/v1/$path",
+				'method' => 'POST',
+				'body' => [ 'type' => 'tex', 'q' => '\sin\newcommand' ]
+			],
+		];
+
+		yield 'Internal Restbase URL case' => [
+			[
+				'MathUseInternalRestbasePath' => true,
+				'VirtualRestConfig' => [
+					'modules' => [
+						'restbase' => [ 'url' => 'http://restbase.test.internal/api/' ]
+					]
+				],
+				'MathFullRestbaseURL' => 'https://myWiki.test/'
+			],
+			[
+				'url' => "http://restbase.test.internal/api/localhost/v1/$path",
+				'method' => 'POST',
+				'body' => [ 'type' => 'tex', 'q' => '\sin\newcommand' ]
+			],
+		];
+
+		yield 'VisualEditor case' => [
+			[
+				'MathFullRestbaseURL' => null,
+				'VisualEditorFullRestbaseURL' => "https://VisualEditor/api/rest_v1/"
+			],
+			[
+				'url' => "https://VisualEditor/api/rest_v1/v1/$path",
+				'method' => 'POST',
+				'body' => [ 'type' => 'tex', 'q' => '\sin\newcommand' ]
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider dataProviderForTestUrlUsedByCheckTeX
+	 */
+	public function testUrlUsedByCheckTeX( array $config, array $expected ) {
+		$response = [
+			'headers' => [
+				'x-resource-location' => 'deadbeef'
+			],
+			'body' => json_encode( [
+				'success' => true,
+				'checked' => 'who cares',
+				'identifiers' => [],
+			] )
+		];
+
+		$this->expectMathRestBaseMockHttpRequest( [ $expected ], [ $response ] );
+
+		$this->overrideConfigValues( $config );
+
+		$input = '\\sin\\newcommand';
+		$rbi = new MathRestbaseInterface( $input );
+
+		$rbi->checkTeX();
+	}
+
+	public function dataProviderForTestUrlUsedByGetML() {
+		$path1 = 'media/math/check/tex';
+		$path2 = 'media/math/render/mml/deadbeef';
+
+		yield 'Math FullRestbaseURL default case' => [
+			[],
+			[
+				[ 'url' => "https://wikimedia.org/api/rest_v1/$path1", 'method' => 'POST' ],
+				[ 'url' => "https://wikimedia.org/api/rest_v1/$path2", 'method' => 'GET' ],
+			],
+		];
+
+		yield 'Math FullRestbaseURL case' => [
+			[
+				'MathFullRestbaseURL' => 'https://myWiki.test/'
+			],
+			[
+				[
+					'url' => "https://myWiki.test/v1/$path1",
+					'method' => 'POST'
+				],
+				[
+					'url' => "https://myWiki.test/v1/$path2",
+					'method' => 'GET'
+				],
+			],
+		];
+
+		yield 'Internal Restbase URL case' => [
+			[
+				'MathUseInternalRestbasePath' => true,
+				'VirtualRestConfig' => [
+					'modules' => [
+						'restbase' => [ 'url' => 'http://restbase.test.internal/api/' ]
+					]
+				],
+			],
+			[
+				[
+					'url' => "http://restbase.test.internal/api/localhost/v1/$path1",
+					'method' => 'POST'
+				],
+				[
+					'url' => "http://restbase.test.internal/api/localhost/v1/$path2",
+					'method' => 'GET'
+				],
+			],
+		];
+
+		yield 'VisualEditor case' => [
+			[
+				'MathFullRestbaseURL' => null,
+				'VisualEditorFullRestbaseURL' => 'https://visual-editor.org/api/rest_v1/'
+			],
+			[
+				[
+					'url' => "https://visual-editor.org/api/rest_v1/v1/$path1",
+					'method' => 'POST'
+				],
+				[
+					'url' => "https://visual-editor.org/api/rest_v1/v1/$path2",
+					'method' => 'GET'
+				],
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider dataProviderForTestUrlUsedByGetML
+	 */
+	public function testUrlUsedByGetML( array $config, array $expectedList ) {
+		$response1 = [
+			'headers' => [
+				'x-resource-location' => 'deadbeef'
+			],
+			'body' => json_encode( [
+				'success' => true,
+				'checked' => 'who cares',
+				'identifiers' => [],
+			] )
+		];
+		$response2 = [
+			'body' => 'who cares'
+		];
+
+		$this->expectMathRestBaseMockHttpRequest( $expectedList, [ $response1, $response2 ] );
+
+		$this->overrideConfigValues( $config );
+
+		$input = '\\sin\\newcommand';
+		$rbi = new MathRestbaseInterface( $input );
+
+		$rbi->getMathML();
+	}
 }
