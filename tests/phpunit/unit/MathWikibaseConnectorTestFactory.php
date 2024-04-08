aa47666a72395a537b7716cabe20362174110a5d
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-04-08 14:45:40 +0200

    Conditionally skip tests that depend on Wikibase
    While investigating the dependency between
    Math and Wikibase, it turned out there was
    a hard dependency in the test code.
    
    This change makes the availability of the Wikibase
    Extension optional and conditionally skips tests.
    
    Bug: T362027
    Change-Id: I9401d7d4a3277b6923dcd81f411ac5a08f1242d2
    (cherry picked from commit 0f11a20d8f35ae2e10ab72e39158d33e4fbf30a0)

--- tests/phpunit/unit/MathWikibaseConnectorTestFactory.php
+++ tests/phpunit/unit/MathWikibaseConnectorTestFactory.php
@@ -3,6 +3,7 @@
 namespace MediaWiki\Extension\Math\Tests;
 
 use DataValues\StringValue;
+use ExtensionRegistry;
 use Language;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Extension\Math\MathFormatter;
@@ -50,6 +51,13 @@ class MathWikibaseConnectorTestFactory extends MediaWikiUnitTestCase {
 				'm'
 			]
 		];
+
+	public static function setUpBeforeClass(): void {
+		if ( !ExtensionRegistry::getInstance()->isLoaded( 'WikibaseClient' ) ) {
+			self::markTestSkipped( 'WikibaseClient is not installed. Skipping tests.' );
+		}
+		parent::setUpBeforeClass();
+	}
 
 	public function getWikibaseConnectorWithExistingItems(
 		EntityRevision $entityRevision,
