e0acf584608f750ef81b73604a991440f03994af
physikerwelt <wiki@physikerwelt.de>
2024-11-22 08:13:37 +0100

    Move cancel callbacks to texutil
    Currently, information regarding individual LaTeX
    macros is distributed in various files. This is the
    first to attempt to collect declarative information
    regarding tex macros in the texutil.json file.
    As a proof of concept we start with only the cancel
    macros.
    
    * Adjust texutil tests so that whitespaces are allowed
      in the payload
    
    Change-Id: Iba2dfd5293ad96d8d8e4e2f555c8df9d1ae4d6ca

--- src/WikiTexVC/TexUtil.php
+++ src/WikiTexVC/TexUtil.php
@@ -11,6 +11,8 @@ use InvalidArgumentException;
  * @method latex_function_names(string $getArg)
  * @method nullary_macro(string $litArg)
  * @method operator(string $getArg)
+ * @method cancel_required(string $getArg)
+ * @method callback(string $getArg)
  */
 class TexUtil {
 	/** @var self|null */
