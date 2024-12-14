5713e1966ec2f399eb122479fa3ee6dfff15c131
physikerwelt <wiki@physikerwelt.de>
2024-12-15 00:42:59 +0100

    Fix rendering of underOver
    We recently changed the internal treatment of macros
     to include the `\` in the macro names.
    The respective code checked if the macro name had the letter 'o'
     at position 0. Now, this is position 1.
    
    * Check for the entire start of over
    * Output an error if the macro name does not star with under
      or over
    * Remove dead code
    
    Bug: T382196
    Change-Id: I8eefd41e27319c8dbb05900ef2581ce4d548d4fe

--- tests/phpunit/unit/WikiTexVC/MMLmappings/BaseParsingTest.php
+++ tests/phpunit/unit/WikiTexVC/MMLmappings/BaseParsingTest.php
@@ -78,28 +78,38 @@ class BaseParsingTest extends TestCase {
 			'\\overline',
 			( new Literal( 'a' ) )
 		);
-		$result = BaseParsing::underover( $node, [], null, 'oXXX', '00AF' );
+		$result = BaseParsing::underover( $node, [], null, '', '00AF' );
 		$this->assertStringStartsWith( '<mrow', $result );
 		$this->assertStringContainsString( 'mover', $result );
 	}
 
 	public function testUnderOverUnder() {
 		$node = new Fun1(
-			'\\overline',
+			'\\underline',
 			( new Literal( 'a' ) )
 		);
-		$result = BaseParsing::underover( $node, [], null, 'uXXX', '00AF' );
+		$result = BaseParsing::underover( $node, [], null, '', '00AF' );
 		$this->assertStringContainsString( 'munder', $result );
 	}
 
 	public function testUnderOverDqUnder() {
-		$node = new DQ(
+		$node = new Fun1(
+			'\\underline', new DQ(
 			( new Literal( 'a' ) ),
 			( new Literal( 'b' ) )
-		);
-		$result = BaseParsing::underover( $node, [], null, 'uXXX', '00AF' );
+		) );
+		$result = BaseParsing::underover( $node, [], null, '', '00AF' );
 		$this->assertStringContainsString( 'munder', $result );
 		$this->assertStringContainsString( 'mrow', $result );
+	}
+
+	public function testUnderOverInvalid() {
+		$node = new Fun1(
+			'\\someline',
+			( new Literal( 'a' ) )
+		);
+		$result = BaseParsing::underover( $node, [], null, '', '00AF' );
+		$this->assertStringContainsString( 'merror', $result );
 	}
 
 	public function testUnderArgPassing() {
@@ -107,7 +117,7 @@ class BaseParsingTest extends TestCase {
 			'\\overline',
 			( new Literal( 'a' ) )
 		);
-		$result = BaseParsing::underover( $node, [ 'k' => 'v' ], null, 'oXXX', '00AF' );
+		$result = BaseParsing::underover( $node, [ 'k' => 'v' ], null, '', '00AF' );
 		$this->assertStringContainsString( '<mi k="v"', $result );
 	}
 
@@ -117,7 +127,7 @@ class BaseParsingTest extends TestCase {
 			( new Literal( 'a' ) )
 		);
 		$result = BaseParsing::underover( $node,
-			[ 'k' => '"<script>alert("problem")</script>"' ], null, 'oXXX', '00AF' );
+			[ 'k' => '"<script>alert("problem")</script>"' ], null, '', '00AF' );
 		$this->assertStringContainsString( 'k="&quot;&lt;script&gt;alert(&quot;problem&quot;)', $result );
 	}
 
