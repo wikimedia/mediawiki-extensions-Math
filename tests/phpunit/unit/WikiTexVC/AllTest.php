394fe28790fc83b3fe96fc30a4666e6339777463
physikerwelt <wiki@physikerwelt.de>
2026-08-19 22:56:53 +0200

    Make \Omicron non upright (like \Chi)
    Bug: T434428
    Change-Id: I5ce4ec0f4936addb7e54f78dc25f77f3dd96915c
    (cherry picked from commit bb88574320524aeb0d931257c427fe13fbc84285)

--- tests/phpunit/unit/WikiTexVC/AllTest.php
+++ tests/phpunit/unit/WikiTexVC/AllTest.php
@@ -263,7 +263,7 @@ class AllTest extends MediaWikiUnitTestCase {
 						'\\leftarrow \\Leftarrow \\Leftarrow \\leq ' .
 						'\\leftrightarrow \\Leftrightarrow \\Leftrightarrow ' .
 						'\\mathrm {M} \\mathbb {N} \\neq \\mathrm {N} \\emptyset ' .
-						'oO\\lor \\partial \\pm ' .
+						'o\\mathrm {O} \\lor \\partial \\pm ' .
 						'\\rightarrow \\Rightarrow \\Rightarrow \\Re \\mathbb {R} ' .
 						'\\mathbb {R} \\upharpoonright \\mathrm {P} \\cdot ' .
 						'\\S \\spadesuit \\subset \\subseteq \\supseteq ' .
