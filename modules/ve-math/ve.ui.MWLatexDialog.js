af581d8bf2480e9e85fe87dbf79faefca0ce1cc9
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2026-09-03 15:40:45 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.31.0 → 0.32.4
    * grunt: 1.6.2 → 1.6.3
    * @puppeteer/browsers: 2.13.0 → 2.13.2
      * https://github.com/advisories/GHSA-jmr9-qjv8-65gv
    * basic-ftp: 5.3.0 → 5.3.1
      * https://github.com/advisories/GHSA-rpmf-866q-6p89
    * brace-expansion: 1.1.13, 2.0.3, 2.1.4, 5.0.5 → 1.1.18, 2.1.4, 5.0.9
      * https://github.com/advisories/GHSA-3jxr-9vmj-r5cp
      * https://github.com/advisories/GHSA-jxxr-4gwj-5jf2
      * https://github.com/advisories/GHSA-mh99-v99m-4gvg
      * https://github.com/advisories/GHSA-rgw5-rvv9-x895
    * fast-uri: 3.0.5 → 3.1.7
      * https://github.com/advisories/GHSA-4c8g-83qw-93j6
      * https://github.com/advisories/GHSA-7p8r-x3mc-p8w7
      * https://github.com/advisories/GHSA-f65p-4m7j-42xc
      * https://github.com/advisories/GHSA-jqff-g426-hqxp
      * https://github.com/advisories/GHSA-q3j6-qgpj-74h6
      * https://github.com/advisories/GHSA-v2hh-gcrm-f6hx
      * https://github.com/advisories/GHSA-v39h-62p7-jpjc
    * fast-xml-builder: 1.1.5 → 1.3.1
      * https://github.com/advisories/GHSA-45c6-75p6-83cc
      * https://github.com/advisories/GHSA-5wm8-gmm8-39j9
    * ip-address: 9.0.5 → 10.7.0
      * https://github.com/advisories/GHSA-mwp4-54f8-5fhr
      * https://github.com/advisories/GHSA-v2v4-37r5-5v8g
    * nanoid: 3.3.11 → 3.3.18
      * https://github.com/advisories/GHSA-28wg-ghj8-5hjv
      * https://github.com/advisories/GHSA-2v37-7h3g-55p8
      * https://github.com/advisories/GHSA-xwg4-73v4-xw9w
    * postcss: 8.5.12 → 8.5.28
      * https://github.com/advisories/GHSA-fxqj-rqcc-2cmp
      * https://github.com/advisories/GHSA-r28c-9q8g-f849
    * socks: 2.8.5 → 2.8.10
      * https://github.com/advisories/GHSA-mwp4-54f8-5fhr
      * https://github.com/advisories/GHSA-v2v4-37r5-5v8g
    * undici: 6.24.1, 7.24.3 → 6.28.0, 7.29.0
      * https://github.com/advisories/GHSA-35p6-xmwp-9g52
      * https://github.com/advisories/GHSA-4cwx-7wf7-3272
      * https://github.com/advisories/GHSA-8xcm-r25x-g524
      * https://github.com/advisories/GHSA-g8m3-5g58-fq7m
      * https://github.com/advisories/GHSA-hm92-r4w5-c3mj
      * https://github.com/advisories/GHSA-jr45-8vmc-qm54
      * https://github.com/advisories/GHSA-m8rv-5g2x-5cg5
      * https://github.com/advisories/GHSA-p88m-4jfj-68fv
      * https://github.com/advisories/GHSA-pr7r-676h-xcf6
      * https://github.com/advisories/GHSA-v3r7-h72x-cjcm
      * https://github.com/advisories/GHSA-vmh5-mc38-953g
      * https://github.com/advisories/GHSA-vxpw-j846-p89q
    * websocket-driver: 0.7.4 → 0.7.5
      * https://github.com/advisories/GHSA-mp7j-qc5w-4988
      * https://github.com/advisories/GHSA-xv26-6w52-cph6
    * ws: 8.18.2 → 8.21.3
      * https://github.com/advisories/GHSA-58qx-3vcg-4xpx
      * https://github.com/advisories/GHSA-96hv-2xvq-fx4p
    
    Additional changes:
    * Disabled Composer audit.block-insecure option (T416518)
    
    Change-Id: Iaadcde5d7746bd4cd5939839e6ddea673ec693dd

--- modules/ve-math/ve.ui.MWLatexDialog.js
+++ modules/ve-math/ve.ui.MWLatexDialog.js
@@ -170,7 +170,6 @@ ve.ui.MWLatexDialog.prototype.initialize = function () {
 				return true;
 			} );
 			symbolData[ category ] = {
-				// eslint-disable-next-line mediawiki/msg-doc
 				label: ve.msg( category ),
 				symbols: symbolList
 			};
